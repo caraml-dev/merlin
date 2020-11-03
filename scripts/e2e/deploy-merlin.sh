f0d9c077225f0c3e1706c07183639c6c297bea97
ariefrahmansyah <ariefrahmansyah@hotmail.com>
2020-11-03 18:32:26 +0700

    Explicitly declare MERLIN_CHART_PATH and MERLIN_IMAGE_TAG for e2e test

--- scripts/e2e/deploy-merlin.sh
+++ scripts/e2e/deploy-merlin.sh
@@ -2,16 +2,14 @@
 
 set -ex
 
-CHART_PATH="$1"
-
-helm install merlin ${CHART_PATH} --namespace=mlp \
-  --values=${CHART_PATH}/values-e2e.yaml \
-  --set merlin.image.tag=${GITHUB_REF#refs/*/} \
+helm install merlin ${MERLIN_CHART_PATH} --namespace=mlp \
+  --values=${MERLIN_CHART_PATH}/values-e2e.yaml \
+  --set merlin.image.tag=${MERLIN_IMAGE_TAG} \
   --dry-run
 
-helm install merlin ${CHART_PATH} --namespace=mlp \
-  --values=${CHART_PATH}/values-e2e.yaml \
-  --set merlin.image.tag=${GITHUB_REF#refs/*/} \
+helm install merlin ${MERLIN_CHART_PATH} --namespace=mlp \
+  --values=${MERLIN_CHART_PATH}/values-e2e.yaml \
+  --set merlin.image.tag=${MERLIN_IMAGE_TAG} \
   --wait --timeout=5m
 
 kubectl get all --namespace=mlp
