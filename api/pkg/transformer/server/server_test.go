1ea9b73f7dd50c8d52f29314c1b7df10f3234453
Pradithya Aria <pradithya.pura@go-jek.com>
2021-02-02 10:48:20 +0800

    Add non success test in server_test.go

--- api/pkg/transformer/server/server_test.go
+++ api/pkg/transformer/server/server_test.go
@@ -41,38 +41,79 @@ func TestServer_PredictHandler_NoTransformation(t *testing.T) {
 }
 
 func TestServer_PredictHandler_WithPreprocess(t *testing.T) {
-	mockPreprocessResponse := []byte(`{"driver_id":"1001","preprocess":true}`)
-	mockPreprocessHandler := func(ctx context.Context, request []byte) ([]byte, error) {
-		return mockPreprocessResponse, nil
+	tests := []struct {
+		name            string
+		request         []byte
+		requestHeader   map[string]string
+		expModelRequest []byte
+		modelResponse   []byte
+		modelStatusCode int
+	}{
+		{
+			"nominal case",
+			[]byte(`{"predictions": [2, 2]}`),
+			map[string]string{MerlinLogIdHeader: "1234"},
+			[]byte(`{"driver_id":"1001","preprocess":true}`),
+			[]byte(`{"predictions": [2, 2]}`),
+			200,
+		},
+		{
+			"model return error",
+			[]byte(`{"predictions": [2, 2]}`),
+			map[string]string{MerlinLogIdHeader: "1234"},
+			[]byte(`{"driver_id":"1001","preprocess":true}`),
+			[]byte(`{"predictions": [2, 2]}`),
+			500,
+		},
 	}
 
-	mockPredictResponse := []byte(`{"predictions": [2, 2]}`)
-	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		body, err := ioutil.ReadAll(r.Body)
-		assert.Nil(t, err)
-		assert.Equal(t, mockPreprocessResponse, body)
-		w.Write(mockPredictResponse)
-	}))
-	defer ts.Close()
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
 
-	rr := httptest.NewRecorder()
+			mockPreprocessHandler := func(ctx context.Context, request []byte) ([]byte, error) {
+				return test.expModelRequest, nil
+			}
 
-	reqBody := bytes.NewBufferString(`{"driver_id":"1001"}`)
-	req, err := http.NewRequest("POST", ts.URL, reqBody)
-	if err != nil {
-		t.Fatal(err)
+			ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+				body, err := ioutil.ReadAll(r.Body)
+				// check log id is propagated as header
+				logId, ok := test.requestHeader[MerlinLogIdHeader]
+				if ok {
+					assert.Equal(t, logId, r.Header.Get(MerlinLogIdHeader))
+				}
+
+				assert.Nil(t, err)
+				assert.Equal(t, test.expModelRequest, body)
+				w.WriteHeader(test.modelStatusCode)
+				w.Write(test.modelResponse)
+			}))
+			defer ts.Close()
+
+			rr := httptest.NewRecorder()
+
+			reqBody := bytes.NewBuffer(test.request)
+			req, err := http.NewRequest("POST", ts.URL, reqBody)
+			for k, v := range test.requestHeader {
+				req.Header.Set(k, v)
+			}
+
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			options := &Options{
+				ModelPredictURL: ts.URL,
+			}
+			logger, _ := zap.NewDevelopment()
+			server := New(options, logger)
+			server.PreprocessHandler = mockPreprocessHandler
+
+			server.PredictHandler(rr, req)
+
+			response, err := ioutil.ReadAll(rr.Body)
+			assert.Nil(t, err)
+			assert.Equal(t, test.modelResponse, response)
+			assert.Equal(t, test.modelStatusCode, rr.Code)
+		})
 	}
-
-	options := &Options{
-		ModelPredictURL: ts.URL,
-	}
-	logger, _ := zap.NewDevelopment()
-	server := New(options, logger)
-	server.PreprocessHandler = mockPreprocessHandler
-
-	server.PredictHandler(rr, req)
-
-	response, err := ioutil.ReadAll(rr.Body)
-	assert.Nil(t, err)
-	assert.Equal(t, mockPredictResponse, response)
 }
