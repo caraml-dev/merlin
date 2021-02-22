5b86ef823cb5d2390e5794f09ca029bf0da44230
Khor Shu Heng <khor.heng@go-jek.com>
2021-02-22 12:15:03 +0800

    Add geohash udf functions

---
+++ api/pkg/transformer/feast/udf.go
@@ -0,0 +1,110 @@
+package feast
+
+import (
+	"errors"
+	"fmt"
+	"github.com/antonmedv/expr"
+	"github.com/antonmedv/expr/vm"
+	"github.com/mmcloughlin/geohash"
+	"github.com/oliveagle/jsonpath"
+	"reflect"
+	"strconv"
+)
+
+type UdfResult struct {
+	Value interface{}
+	Error error
+}
+
+type UdfEnv struct {
+	UnmarshalledJsonRequest interface{}
+}
+
+func (env UdfEnv) Geohash(latitudeJsonPath, longitudeJsonPath string) UdfResult {
+	value, err := extractGeohash(env.UnmarshalledJsonRequest, latitudeJsonPath, longitudeJsonPath)
+	return UdfResult{
+		Value: value,
+		Error: err,
+	}
+}
+
+func toList(o interface{}) []interface{} {
+	var listInterface []interface{}
+	switch o.(type) {
+	case []interface{}:
+		listInterface = o.([]interface{})
+	case interface{}:
+		listInterface = []interface{}{o}
+	}
+	return listInterface
+}
+
+func toFloat64(o interface{}) (float64, error) {
+	var floatValue float64
+	switch o.(type) {
+	case float64:
+		floatValue = o.(float64)
+	case string:
+		parsed, err := strconv.ParseFloat(o.(string), 64)
+		if err != nil {
+			return 0, err
+		}
+		floatValue = parsed
+	case int:
+		floatValue = float64(o.(int))
+	default:
+		return 0, errors.New(fmt.Sprintf("%v cannot be parsed into float64", reflect.TypeOf(o)))
+	}
+	return floatValue, nil
+
+}
+
+func extractGeohash(jsonRequestBody interface{}, latitudeJsonPath, longitudeJsonPath string) (interface{}, error) {
+	latitude, err := jsonpath.JsonPathLookup(jsonRequestBody, latitudeJsonPath)
+	if err != nil {
+		return nil, err
+	}
+	latitudes := toList(latitude)
+	if len(latitudes) == 0 {
+		return nil, errors.New("empty arrays of latitudes provided")
+	}
+
+	longitude, err := jsonpath.JsonPathLookup(jsonRequestBody, longitudeJsonPath)
+	if err != nil {
+		return nil, err
+	}
+	longitudes := toList(longitude)
+	if len(longitudes) == 0 {
+		return nil, errors.New("empty arrays of longitudes provided")
+	}
+
+	geohashes := make([]string, 0)
+	for _, lat := range latitudes {
+		for _, long := range longitudes {
+			latFloat, err := toFloat64(lat)
+			if err != nil {
+				return nil, err
+			}
+			longFloat, err := toFloat64(long)
+			if err != nil {
+				return nil, err
+			}
+			geohashes = append(geohashes, geohash.Encode(latFloat, longFloat))
+		}
+	}
+
+	if len(geohashes) == 1 {
+		return geohashes[0], nil
+	} else {
+		return geohashes, nil
+	}
+
+}
+
+func mustCompileUdf(udfString string) *vm.Program {
+	compiled, err := expr.Compile(udfString, expr.Env(UdfEnv{}))
+	if err != nil {
+		panic(err)
+	}
+	return compiled
+}
---
+++ api/pkg/transformer/feast/udf_test.go
@@ -0,0 +1,98 @@
+package feast
+
+import (
+	"encoding/json"
+	"errors"
+	"github.com/mmcloughlin/geohash"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestExtractGeohash(t *testing.T) {
+	testJsonString := []byte(`{
+		"latitude" : 1.0,
+		"latitudeString": "1.0",
+		"latitudeWrongType": "abcde",
+		"location": {
+			"latitude": 0.1,
+			"longitude": 2.0
+		},
+		"latitudeArrays": [1.0, 2.0],
+		"longitude" : 2.0,
+		"longitudeInteger": 1,
+		"longitudeArrays": [1.0, 2.0]
+	}`)
+	var testJsonUnmarshallled interface{}
+	err := json.Unmarshal(testJsonString, &testJsonUnmarshallled)
+	if err != nil {
+		panic(err)
+	}
+
+	tests := []struct {
+		name              string
+		latitudeJsonPath  string
+		longitudeJsonPath string
+		expValue          interface{}
+		expError          error
+	}{
+		{
+			name:              "geohash from json fields",
+			latitudeJsonPath:  "$.latitude",
+			longitudeJsonPath: "$.longitude",
+			expValue:          geohash.Encode(1.0, 2.0),
+		},
+		{
+			name:              "geohash from json struct",
+			latitudeJsonPath:  "$.location.latitude",
+			longitudeJsonPath: "$.location.longitude",
+			expValue:          geohash.Encode(0.1, 2.0),
+		},
+		{
+			name:              "type conversion for latitude and longitude input",
+			latitudeJsonPath:  "$.latitudeString",
+			longitudeJsonPath: "$.longitudeInteger",
+			expValue:          geohash.Encode(1.0, 1.0),
+		},
+		{
+			name:              "type conversion error",
+			latitudeJsonPath:  "$.latitudeWrongType",
+			longitudeJsonPath: "$.longitude",
+			expError:          errors.New("strconv.ParseFloat: parsing \"abcde\": invalid syntax"),
+		},
+		{
+			name:              "latitude arrays and single longitude",
+			latitudeJsonPath:  "$.latitudeArrays",
+			longitudeJsonPath: "$.longitude",
+			expValue: []string{
+				geohash.Encode(1.0, 2.0),
+				geohash.Encode(2.0, 2.0),
+			},
+		},
+		{
+			name:              "latitude and longitude arrays",
+			latitudeJsonPath:  "$.latitudeArrays",
+			longitudeJsonPath: "$.longitudeArrays",
+			expValue: []string{
+				geohash.Encode(1.0, 1.0),
+				geohash.Encode(1.0, 2.0),
+				geohash.Encode(2.0, 1.0),
+				geohash.Encode(2.0, 2.0),
+			},
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			actual, err := extractGeohash(testJsonUnmarshallled, test.latitudeJsonPath, test.longitudeJsonPath)
+			if err != nil {
+				if test.expError != nil {
+					assert.EqualError(t, err, test.expError.Error())
+					return
+				} else {
+					assert.Fail(t, err.Error())
+				}
+			}
+			assert.Equal(t, test.expValue, actual)
+		})
+	}
+}
