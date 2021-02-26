cd359dd8ecfecf2e032aeb48364ef4cf738e03e9
Choo Jia Le <5955220+jials@users.noreply.github.com>
2021-02-26 15:13:05 +0800

    Fix deploy-merlin.sh tag name

--- scripts/e2e/deploy-merlin.sh
+++ scripts/e2e/deploy-merlin.sh
@@ -6,12 +6,12 @@ CHART_PATH="$1"
 
 helm install merlin ${CHART_PATH} --namespace=mlp \
   --values=${CHART_PATH}/values-e2e.yaml \
-  --set merlin.image.tag=${GITHUB_REF#refs/*/} \
+  --set merlin.image.tag=${GITHUB_HEAD_REF} \
   --dry-run
 
 helm install merlin ${CHART_PATH} --namespace=mlp \
   --values=${CHART_PATH}/values-e2e.yaml \
-  --set merlin.image.tag=${GITHUB_REF#refs/*/} \
+  --set merlin.image.tag=${GITHUB_HEAD_REF} \
   --wait --timeout=5m
 
 kubectl get all --namespace=mlp
