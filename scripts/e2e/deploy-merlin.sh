7facf6ec2c25fae85554c5c42c046e21c90f111d
Choo Jia Le <5955220+jials@users.noreply.github.com>
2021-03-01 16:52:41 +0800

    Get tag from github_ref if action is triggered by push

--- scripts/e2e/deploy-merlin.sh
+++ scripts/e2e/deploy-merlin.sh
@@ -6,12 +6,12 @@ CHART_PATH="$1"
 
 helm install merlin ${CHART_PATH} --namespace=mlp \
   --values=${CHART_PATH}/values-e2e.yaml \
-  --set merlin.image.tag=${GITHUB_HEAD_REF} \
+  --set merlin.image.tag=${GITHUB_HEAD_REF:-${GITHUB_REF#refs/*/}} \
   --dry-run
 
 helm install merlin ${CHART_PATH} --namespace=mlp \
   --values=${CHART_PATH}/values-e2e.yaml \
-  --set merlin.image.tag=${GITHUB_HEAD_REF} \
+  --set merlin.image.tag=${GITHUB_HEAD_REF:-${GITHUB_REF#refs/*/}} \
   --wait --timeout=5m
 
 kubectl get all --namespace=mlp
