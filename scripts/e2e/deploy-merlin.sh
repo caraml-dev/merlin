77083915f5cfa946d34428ab8ffb9590ecb08fd3
Tio Pramayudi <tio.pramayudi@go-jek.com>
2021-03-04 11:03:05 +0700

    Remove dry-run when installing merlin

--- scripts/e2e/deploy-merlin.sh
+++ scripts/e2e/deploy-merlin.sh
@@ -21,24 +21,6 @@ helm install --debug merlin ${CHART_PATH} --namespace=mlp --values=${CHART_PATH}
   --set mlflow.ingress.host=merlin-mlflow.mlp.${INGRESS_HOST}.nip.io \
   --set mlflow.extraEnvs.MLFLOW_S3_ENDPOINT_URL=http://minio.minio.svc.cluster.local:9000 \
   --set mlflow.ingress.path="/*" \
-  --set mlflow.postgresql.requests.cpu="25m" \
-  --set mlflow.postgresql.requests.memory="256Mi" \
-  --timeout=5m \
-  --dry-run
-
-helm install --debug merlin ${CHART_PATH} --namespace=mlp --values=${CHART_PATH}/values-e2e.yaml \
-  --set merlin.image.tag=${MERLIN_VERSION} \
-  --set merlin.apiHost=http://merlin.mlp.${INGRESS_HOST}.nip.io/v1 \
-  --set merlin.mlpApi.apiHost=http://mlp.mlp.svc.cluster.local:8080/v1 \
-  --set merlin.ingress.enabled=true \
-  --set merlin.ingress.class=istio \
-  --set merlin.ingress.host=merlin.mlp.${INGRESS_HOST}.nip.io \
-  --set merlin.ingress.path="/*" \
-  --set mlflow.ingress.enabled=true \
-  --set mlflow.ingress.class=istio \
-  --set mlflow.ingress.host=merlin-mlflow.mlp.${INGRESS_HOST}.nip.io \
-  --set mlflow.extraEnvs.MLFLOW_S3_ENDPOINT_URL=http://minio.minio.svc.cluster.local:9000 \
-  --set mlflow.ingress.path="/*" \
   --set mlflow.resources.requests.cpu="25m" \
   --set mlflow.resources.requests.memory="256Mi" \
   --timeout=5m \
@@ -47,6 +29,9 @@ helm install --debug merlin ${CHART_PATH} --namespace=mlp --values=${CHART_PATH}
 
 kubectl set env deployment/merlin -n mlp MLFLOW_TRACKING_URL="http://${HOST_IP}:31100"
 
+########################################
+# Patch MLFLOW service so it is accessible outside k8s cluster
+#
 cat <<EOF > ./patch-merlin-mlflow-nodeport.yaml
 spec:
   type: NodePort
@@ -60,6 +45,9 @@ EOF
 kubectl patch service/merlin-mlflow -n mlp --patch="$(cat patch-merlin-mlflow-nodeport.yaml)"
 sleep 12
 
+########################################
+# Create dummy logger service
+#
 cat <<EOF > ./logger-sample.yaml
 apiVersion: serving.knative.dev/v1alpha1
 kind: Service
