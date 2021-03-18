20844a6fcaf4c32392a681a1661cd37dc47a580a
ariefrahmansyah <ariefrahmansyah@hotmail.com>
2021-03-18 11:10:55 +0700

    Use merlin-test's feast entities and features

--- python/sdk/test/transformer_test.py
+++ python/sdk/test/transformer_test.py
@@ -19,7 +19,7 @@ from merlin.resource_request import ResourceRequest
 
 @pytest.mark.unit
 def test_standard_transformer():
-    transformer_config_path = os.path.join("test/transformer", "feast_standard_transformer.yaml")
+    transformer_config_path = os.path.join("test/transformer", "standard_transformer.yaml")
     transformer = StandardTransformer(config_file=transformer_config_path, enabled=False)
     assert transformer.env_vars == {'STANDARD_TRANSFORMER_CONFIG': '{"transformerConfig": {"feast": [{"entities": [{"name": "customer_id", "valueType": "STRING", "jsonPath": "$.customer_id"}], "features": [{"name": "total_booking_1w", "defaultValue": "0.0"}, {"name": "total_booking_4w", "defaultValue": "0.0"}]}, {"entities": [{"name": "merchant_id", "valueType": "STRING", "jsonPath": "$.merchant_id"}], "features": [{"name": "total_completed_order_1w", "defaultValue": "0.0"}, {"name": "avg_completed_order_1w", "defaultValue": "0.0"}]}]}}'}
     assert not transformer.enabled
@@ -28,7 +28,7 @@ def test_standard_transformer():
     assert transformer.transformer_type == TransformerType.STANDARD_TRANSFORMER
 
 def test_standard_transformer_with_env_vars():
-    transformer_config_path = os.path.join("test/transformer", "feast_standard_transformer.yaml")
+    transformer_config_path = os.path.join("test/transformer", "standard_transformer.yaml")
     resource = ResourceRequest(min_replica=1, max_replica=2, cpu_request="100m", memory_request="128Mi")
     transformer = StandardTransformer(config_file=transformer_config_path,
                                       enabled=True,
