0b1f0559358c88ed746cd7ba94afc8bf53f3e9a4
Pradithya Aria <pradithya.pura@go-jek.com>
2021-04-12 10:28:03 +0800

    Include ColumnTypes in FeatureTable

--- api/pkg/transformer/types/feature_table.go
+++ api/pkg/transformer/types/feature_table.go
@@ -1,11 +1,21 @@
 package types
 
+import (
+	"fmt"
+
+	"github.com/feast-dev/feast/sdk/go/protos/feast/types"
+
+	"github.com/gojek/merlin/pkg/transformer/types/series"
+	"github.com/gojek/merlin/pkg/transformer/types/table"
+)
+
 type ValueRow []interface{}
 
 type ValueRows []ValueRow
 
 type FeatureTable struct {
-	Name    string    `json:"-"`
-	Columns []string  `json:"columns"`
-	Data    ValueRows `json:"data"`
+	Name        string                 `json:"-"`
+	Columns     []string               `json:"columns"`
+	ColumnTypes []types.ValueType_Enum `json:"-"`
+	Data        ValueRows              `json:"data"`
 }
