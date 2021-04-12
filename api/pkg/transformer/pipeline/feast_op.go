ee38adeccb12550f214fc5968a18dbcc1d0345fe
Pradithya Aria <pradithya.pura@go-jek.com>
2021-04-12 10:50:26 +0800

    Implement FeastOp

--- api/pkg/transformer/pipeline/feast_op.go
+++ api/pkg/transformer/pipeline/feast_op.go
@@ -1,17 +1,47 @@
 package pipeline
 
-import "github.com/gojek/merlin/pkg/transformer/spec"
+import (
+	"context"
+
+	"go.uber.org/zap"
+
+	feastSdk "github.com/feast-dev/feast/sdk/go"
+
+	"github.com/gojek/merlin/pkg/transformer/cache"
+	"github.com/gojek/merlin/pkg/transformer/feast"
+	"github.com/gojek/merlin/pkg/transformer/spec"
+)
 
 type FeastOp struct {
-	featureTables []*spec.FeatureTable
+	feastRetriever feast.FeatureRetriever
+	logger         *zap.Logger
 }
 
-func NewFeastOp(featureTables []*spec.FeatureTable) Op {
+func NewFeastOp(feastClient feastSdk.Client, feastOptions *feast.Options, cache cache.Cache, entityExtractor *feast.EntityExtractor, featureTableSpecs []*spec.FeatureTable, logger *zap.Logger) Op {
+	feastRetriever := feast.NewFeastRetriever(
+		feastClient,
+		entityExtractor,
+		featureTableSpecs,
+		feastOptions,
+		cache,
+		logger,
+	)
+
 	return &FeastOp{
-		featureTables: featureTables,
+		feastRetriever: feastRetriever,
+		logger:         logger,
 	}
 }
 
-func (f FeastOp) Execute(environment *Environment) error {
-	panic("implement me")
+func (op *FeastOp) Execute(context context.Context, environment *Environment) error {
+	featureTables, err := op.feastRetriever.RetrieveFeatureOfEntityInSymbolRegistry(context, environment.SymbolRegistry())
+	if err != nil {
+		return err
+	}
+
+	for _, featureTable := range featureTables {
+		environment.SetSymbol(featureTable.Name, featureTable.AsTable())
+	}
+
+	return nil
 }
---
+++ api/pkg/transformer/pipeline/feast_op_test.go
@@ -0,0 +1,166 @@
+package pipeline
+
+import (
+	"context"
+	"testing"
+	"time"
+
+	"github.com/feast-dev/feast/sdk/go/protos/feast/types"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+	"go.uber.org/zap"
+
+	"github.com/gojek/merlin/pkg/transformer/feast/mocks"
+	"github.com/gojek/merlin/pkg/transformer/symbol"
+	transTypes "github.com/gojek/merlin/pkg/transformer/types"
+	"github.com/gojek/merlin/pkg/transformer/types/series"
+	"github.com/gojek/merlin/pkg/transformer/types/table"
+)
+
+func TestFeastOp_Execute(t *testing.T) {
+	sr := symbol.NewRegistry()
+
+	type mockFeastRetriever struct {
+		result []*transTypes.FeatureTable
+		error  error
+	}
+	type args struct {
+		context     context.Context
+		environment *Environment
+	}
+	tests := []struct {
+		name               string
+		mockFeastRetriever mockFeastRetriever
+		args               args
+		expVariables       map[string]interface{}
+		wantErr            bool
+	}{
+		{
+			name: "produce one table",
+			mockFeastRetriever: mockFeastRetriever{
+				result: []*transTypes.FeatureTable{
+					{
+						Name: "driver_table",
+						Columns: []string{
+							"driver_id",
+							"feature_a",
+							"feature_b",
+						},
+						Data: transTypes.ValueRows{
+							{
+								"1111", 11.11, 12,
+							},
+							{
+								"2222", 22.22, 22,
+							},
+						},
+						ColumnTypes: []types.ValueType_Enum{types.ValueType_STRING, types.ValueType_DOUBLE, types.ValueType_INT64},
+					},
+				},
+				error: nil,
+			},
+			args: args{
+				context: context.Background(),
+				environment: &Environment{
+					symbolRegistry: sr,
+				},
+			},
+			expVariables: map[string]interface{}{
+				"driver_table": table.New(
+					series.New([]string{"1111", "2222"}, series.String, "driver_id"),
+					series.New([]float64{11.11, 22.22}, series.Float, "feature_a"),
+					series.New([]float64{12, 22}, series.Int, "feature_b")),
+			},
+			wantErr: false,
+		},
+		{
+			name: "produce two table",
+			mockFeastRetriever: mockFeastRetriever{
+				result: []*transTypes.FeatureTable{
+					{
+						Name: "driver_table",
+						Columns: []string{
+							"driver_id",
+							"feature_a",
+							"feature_b",
+						},
+						Data: transTypes.ValueRows{
+							{
+								"1111", 11.11, 12,
+							},
+							{
+								"2222", 22.22, 22,
+							},
+						},
+						ColumnTypes: []types.ValueType_Enum{types.ValueType_STRING, types.ValueType_DOUBLE, types.ValueType_INT64},
+					},
+					{
+						Name: "customer_table",
+						Columns: []string{
+							"driver_id",
+							"feature_a",
+							"feature_b",
+						},
+						Data: transTypes.ValueRows{
+							{
+								"1111", 11.11, 12,
+							},
+							{
+								"2222", 22.22, 22,
+							},
+						},
+						ColumnTypes: []types.ValueType_Enum{types.ValueType_STRING, types.ValueType_DOUBLE, types.ValueType_INT64},
+					},
+				},
+				error: nil,
+			},
+			args: args{
+				context: context.Background(),
+				environment: &Environment{
+					symbolRegistry: sr,
+				},
+			},
+			expVariables: map[string]interface{}{
+				"driver_table": table.New(
+					series.New([]string{"1111", "2222"}, series.String, "driver_id"),
+					series.New([]float64{11.11, 22.22}, series.Float, "feature_a"),
+					series.New([]float64{12, 22}, series.Int, "feature_b")),
+				"customer_table": table.New(
+					series.New([]string{"1111", "2222"}, series.String, "driver_id"),
+					series.New([]float64{11.11, 22.22}, series.Float, "feature_a"),
+					series.New([]float64{12, 22}, series.Int, "feature_b")),
+			},
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+
+			logger, err := zap.NewDevelopment()
+			if err != nil {
+				panic(err)
+			}
+
+			mockFeastRetriever := &mocks.FeatureRetriever{}
+			mockFeastRetriever.On("RetrieveFeatureOfEntityInSymbolRegistry", mock.Anything, tt.args.environment.symbolRegistry).
+				Return(tt.mockFeastRetriever.result, tt.mockFeastRetriever.error)
+
+			op := &FeastOp{
+				feastRetriever: mockFeastRetriever,
+				logger:         logger,
+			}
+			if err := op.Execute(tt.args.context, tt.args.environment); (err != nil) != tt.wantErr {
+				t.Errorf("Execute() error = %v, wantErr %v", err, tt.wantErr)
+			}
+
+			for varName, varValue := range tt.expVariables {
+				switch v := varValue.(type) {
+				case time.Time:
+					assert.True(t, v.Sub(sr[varName].(time.Time)) < time.Second)
+				default:
+					assert.Equal(t, v, sr[varName])
+				}
+			}
+		})
+	}
+}
