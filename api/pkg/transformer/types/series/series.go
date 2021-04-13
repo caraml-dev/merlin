1548941459c06a063110bbe858417388e568d935
Pradithya Aria <pradithya.pura@go-jek.com>
2021-04-13 14:20:45 +0800

    Ass Series.NewInferType method

--- api/pkg/transformer/types/series/series.go
+++ api/pkg/transformer/types/series/series.go
@@ -1,6 +1,13 @@
 package series
 
-import "github.com/go-gota/gota/series"
+import (
+	"fmt"
+	"reflect"
+
+	"github.com/go-gota/gota/series"
+
+	"github.com/gojek/merlin/pkg/transformer/types/converter"
+)
 
 type Type string
 
@@ -24,10 +31,120 @@ func New(values interface{}, t Type, name string) *Series {
 	return &Series{&s}
 }
 
+func NewInferType(values interface{}, seriesName string) (*Series, error) {
+	seriesType := detectType(values)
+	seriesValues, err := castValues(values, seriesType)
+	if err != nil {
+		return nil, err
+	}
+
+	return New(seriesValues, seriesType, seriesName), nil
+}
+
 func (s *Series) Series() *series.Series {
 	return s.series
 }
 
 func (s *Series) Type() Type {
 	return Type(s.series.Type())
+}
+
+func detectType(values interface{}) Type {
+	var hasBool, hasString, hasInt, hasFloat bool
+	v := reflect.ValueOf(values)
+	switch v.Kind() {
+	case reflect.Slice:
+		for i := 0; i < v.Len(); i++ {
+			if v.Index(i).Interface() == nil {
+				continue
+			}
+
+			switch v.Index(i).Interface().(type) {
+			case float64, float32:
+				hasFloat = true
+			case int, int8, int16, int32, int64:
+				hasInt = true
+			case bool:
+				hasBool = true
+			default:
+				hasString = true
+			}
+		}
+	default:
+		switch values.(type) {
+		case float64, float32:
+			hasFloat = true
+		case int, int8, int16, int32, int64:
+			hasInt = true
+		case bool:
+			hasBool = true
+		default:
+			hasString = true
+		}
+	}
+
+	switch {
+	case hasString:
+		return String
+	case hasBool:
+		return Bool
+	case hasFloat:
+		return Float
+	case hasInt:
+		return Int
+	default:
+		return String
+	}
+}
+
+func castValues(values interface{}, colType Type) ([]interface{}, error) {
+	v := reflect.ValueOf(values)
+	var seriesValues []interface{}
+	switch v.Kind() {
+	case reflect.Slice:
+		seriesValues = make([]interface{}, v.Len())
+		for i := 0; i < v.Len(); i++ {
+			iVal := v.Index(i).Interface()
+			if iVal == nil {
+				seriesValues[i] = iVal
+				continue
+			}
+			cVal, err := castValue(iVal, colType)
+			if err != nil {
+				return nil, err
+			}
+
+			seriesValues[i] = cVal
+		}
+	default:
+		seriesValues = make([]interface{}, 1)
+		iVal := v.Interface()
+		if iVal == nil {
+			return seriesValues, nil
+		}
+
+		v, err := castValue(iVal, colType)
+		if err != nil {
+			return nil, err
+		}
+
+		seriesValues[0] = v
+	}
+
+	return seriesValues, nil
+}
+
+func castValue(singleValue interface{}, seriesType Type) (interface{}, error) {
+	switch seriesType {
+	case Int:
+		return converter.ToInt(singleValue)
+	case Float:
+		return converter.ToFloat64(singleValue)
+	case Bool:
+		return converter.ToBool(singleValue)
+	case String:
+		return converter.ToString(singleValue)
+	default:
+		return nil, fmt.Errorf("unknown series type %s", seriesType)
+	}
 }
--- api/pkg/transformer/types/series/series_test.go
+++ api/pkg/transformer/types/series/series_test.go
@@ -18,3 +18,163 @@ func TestSeries_New(t *testing.T) {
 	assert.Equal(t, String, s2.Type())
 	assert.Equal(t, *s2.Series(), gotaSeries)
 }
+
+func TestSeries_NewInferType(t *testing.T) {
+	type args struct {
+		values     interface{}
+		seriesName string
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    *Series
+		wantErr bool
+	}{
+		{
+			name: "single value string",
+			args: args{
+				values:     "1111",
+				seriesName: "string_series",
+			},
+			want:    New([]interface{}{"1111"}, String, "string_series"),
+			wantErr: false,
+		},
+		{
+			name: "string array without nil",
+			args: args{
+				values:     []string{"1111", "2222", "3333", "4444"},
+				seriesName: "string_series",
+			},
+			want:    New([]string{"1111", "2222", "3333", "4444"}, String, "string_series"),
+			wantErr: false,
+		},
+		{
+			name: "string array with nil",
+			args: args{
+				values:     []interface{}{"1111", "2222", "3333", "4444", nil},
+				seriesName: "string_series",
+			},
+			want:    New([]interface{}{"1111", "2222", "3333", "4444", nil}, String, "string_series"),
+			wantErr: false,
+		},
+		{
+			name: "string array with nil and mixed type",
+			args: args{
+				values:     []interface{}{"1111", false, 33.33, 4444, nil},
+				seriesName: "string_series",
+			},
+			want:    New([]interface{}{"1111", "false", "33.33", "4444", nil}, String, "string_series"),
+			wantErr: false,
+		},
+		{
+			name: "single value int",
+			args: args{
+				values:     int64(1),
+				seriesName: "int_series",
+			},
+			want:    New([]interface{}{1}, Int, "int_series"),
+			wantErr: false,
+		},
+		{
+			name: "int array without nil",
+			args: args{
+				values:     []int64{1111, 2222, 3333, 4444},
+				seriesName: "int_series",
+			},
+			want:    New([]int{1111, 2222, 3333, 4444}, Int, "int_series"),
+			wantErr: false,
+		},
+		{
+			name: "int array with nil",
+			args: args{
+				values:     []interface{}{1111, 2222, 3333, 4444, nil},
+				seriesName: "int_series",
+			},
+			want:    New([]interface{}{1111, 2222, 3333, 4444, nil}, Int, "int_series"),
+			wantErr: false,
+		},
+		{
+			name: "int array with nil and mixed type",
+			args: args{
+				values:     []interface{}{int8(11), int16(2222), int32(3333), int64(4444), nil},
+				seriesName: "int_series",
+			},
+			want:    New([]interface{}{11, 2222, 3333, 4444, nil}, Int, "int_series"),
+			wantErr: false,
+		},
+		{
+			name: "single value float",
+			args: args{
+				values:     float64(1.1),
+				seriesName: "float_series",
+			},
+			want:    New([]interface{}{1.1}, Float, "float_series"),
+			wantErr: false,
+		},
+		{
+			name: "float array without nil",
+			args: args{
+				values:     []float64{1111.11, 2222.22, 3333.33, 4444.44},
+				seriesName: "float_series",
+			},
+			want:    New([]float64{1111.11, 2222.22, 3333.33, 4444.44}, Float, "float_series"),
+			wantErr: false,
+		},
+		{
+			name: "float array with nil",
+			args: args{
+				values:     []interface{}{1111.11, 2222.22, 3333.33, 4444.44, nil},
+				seriesName: "float_series",
+			},
+			want:    New([]interface{}{1111.11, 2222.22, 3333.33, 4444.44, nil}, Float, "float_series"),
+			wantErr: false,
+		},
+		{
+			name: "float array with nil and mixed types",
+			args: args{
+				values:     []interface{}{float32(1111), 2222.22, int8(1), int16(16), int32(1234), int64(555555), nil},
+				seriesName: "float_series",
+			},
+			want:    New([]interface{}{1111, 2222.22, 1, 16, 1234, 555555, nil}, Float, "float_series"),
+			wantErr: false,
+		},
+		{
+			name: "single value bool",
+			args: args{
+				values:     true,
+				seriesName: "bool_series",
+			},
+			want:    New([]bool{true}, Bool, "bool_series"),
+			wantErr: false,
+		},
+		{
+			name: "bool array without nil",
+			args: args{
+				values:     []bool{true, false},
+				seriesName: "bool_series",
+			},
+			want:    New([]bool{true, false}, Bool, "bool_series"),
+			wantErr: false,
+		},
+		{
+			name: "bool array with nil",
+			args: args{
+				values:     []interface{}{true, false, nil},
+				seriesName: "bool_series",
+			},
+			want:    New([]interface{}{true, false, nil}, Bool, "bool_series"),
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := NewInferType(tt.args.values, tt.args.seriesName)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("NewInferType() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+
+			assert.Equal(t, tt.want, got)
+		})
+	}
+}
