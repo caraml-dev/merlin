f28b70634cd7508c84c5468e334120c281fabf0e
Tio Pramayudi <tio.pramayudi@gojek.com>
2021-04-28 11:21:48 +0700

    Add additional built-in function and compact standard transformer configuration env variable

--- api/pkg/transformer/symbol/function/geospatial.go
+++ api/pkg/transformer/symbol/function/geospatial.go
@@ -3,12 +3,18 @@ package function
 import (
 	"errors"
 	"fmt"
+	"math"
 	"reflect"
 
 	"github.com/golang/geo/s2"
 	"github.com/mmcloughlin/geohash"
 
 	"github.com/gojek/merlin/pkg/transformer/types/converter"
+)
+
+const (
+	earthRaidusKm = 6371 // radius of the earth in kilometers.
+	pointFive     = 0.5
 )
 
 type LatLong struct {
@@ -64,6 +70,121 @@ func S2ID(lat interface{}, lon interface{}, level int) (interface{}, error) {
 	default:
 		return nil, fmt.Errorf("unknown type: %T", latLong)
 	}
+}
+
+func PolarAngle(lat1 interface{}, lon1 interface{}, lat2 interface{}, lon2 interface{}) (interface{}, error) {
+	firstPoint, err := extractLatLong(lat1, lon1)
+	if err != nil {
+		return nil, err
+	}
+
+	secondPoint, err := extractLatLong(lat2, lon2)
+	if err != nil {
+		return nil, err
+	}
+
+	switch firstPoint.(type) {
+	case []*LatLong:
+		firstPoints := firstPoint.([]*LatLong)
+		secondPoints, ok := secondPoint.([]*LatLong)
+		if !ok {
+			return nil, errors.New("first point and second point has different format")
+		}
+		if len(secondPoints) != len(firstPoints) {
+			return nil, errors.New("both first point and second point arrays must have the same length")
+		}
+		var values []interface{}
+		for idx, point1 := range firstPoints {
+			point2 := secondPoints[idx]
+			values = append(values, calculatePolarAngle(point1, point2))
+		}
+		return values, nil
+
+	case *LatLong:
+		firstPoint := firstPoint.(*LatLong)
+		secondPoint, ok := secondPoint.(*LatLong)
+		if !ok {
+			return nil, errors.New("first point and second point has different format")
+		}
+		value := calculatePolarAngle(firstPoint, secondPoint)
+		return value, nil
+
+	default:
+		return nil, fmt.Errorf("unknown type: %T", firstPoint)
+	}
+}
+
+func HaversineDistance(lat1 interface{}, lon1 interface{}, lat2 interface{}, lon2 interface{}) (interface{}, error) {
+	firstPoint, err := extractLatLong(lat1, lon1)
+	if err != nil {
+		return nil, err
+	}
+
+	secondPoint, err := extractLatLong(lat2, lon2)
+	if err != nil {
+		return nil, err
+	}
+
+	switch firstPoint.(type) {
+	case []*LatLong:
+		firstPoints := firstPoint.([]*LatLong)
+		secondPoints, ok := secondPoint.([]*LatLong)
+		if !ok {
+			return nil, errors.New("first point and second point has different format")
+		}
+		if len(secondPoints) != len(firstPoints) {
+			return nil, errors.New("both first point and second point arrays must have the same length")
+		}
+		var values []interface{}
+		for idx, point1 := range firstPoints {
+			point2 := secondPoints[idx]
+			values = append(values, calculateDistance(point1, point2))
+		}
+		return values, nil
+
+	case *LatLong:
+		firstPoint := firstPoint.(*LatLong)
+		secondPoint, ok := secondPoint.(*LatLong)
+		if !ok {
+			return nil, errors.New("first point and second point has different format")
+		}
+		value := calculateDistance(firstPoint, secondPoint)
+		return value, nil
+
+	default:
+		return nil, fmt.Errorf("unknown type: %T", firstPoint)
+	}
+}
+
+func degreesToRadians(d float64) float64 {
+	return d * math.Pi / 180
+}
+
+func calculateDistance(firstPoint *LatLong, secondPoint *LatLong) float64 {
+	lat1 := degreesToRadians(firstPoint.lat)
+	lon1 := degreesToRadians(firstPoint.long)
+	lat2 := degreesToRadians(secondPoint.lat)
+	lon2 := degreesToRadians(secondPoint.long)
+
+	diffLat := lat2 - lat1
+	diffLon := lon2 - lon1
+
+	a := math.Pow(math.Sin(diffLat/2), 2) + math.Cos(lat1)*math.Cos(lat2)*
+		math.Pow(math.Sin(diffLon/2), 2)
+
+	c := 2 * math.Atan2(math.Sqrt(a), math.Sqrt(1-a))
+	return c * earthRaidusKm
+}
+
+func calculatePolarAngle(firstPoint *LatLong, secondPoint *LatLong) float64 {
+	lat1 := degreesToRadians(firstPoint.lat)
+	lon1 := degreesToRadians(firstPoint.long)
+	lat2 := degreesToRadians(secondPoint.lat)
+	lon2 := degreesToRadians(secondPoint.long)
+
+	y := lat2 - lat1
+	x := (lon2 - lon1) * math.Cos(pointFive*(lat2+lat1))
+	return math.Atan2(y, x)
 }
 
 func extractLatLong(latitude, longitude interface{}) (interface{}, error) {
