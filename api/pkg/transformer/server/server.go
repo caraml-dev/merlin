3ab943d682d0a89dda8830ede02910360cd626eb
Pradithya Aria <pradithya.pura@go-jek.com>
2021-04-30 18:12:14 +0800

    Enable pprof

--- api/pkg/transformer/server/server.go
+++ api/pkg/transformer/server/server.go
@@ -6,7 +6,7 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
-	_ "net/http/pprof"
+	"net/http/pprof"
 	"os"
 	"os/signal"
 	"strings"
@@ -161,6 +161,11 @@ func (s *Server) Run() {
 	health := healthcheck.NewHandler()
 	s.router.Handle("/", health)
 	s.router.Handle("/metrics", promhttp.Handler())
+	s.router.HandleFunc("/debug/pprof/", pprof.Index)
+	s.router.HandleFunc("/debug/pprof/cmdline", pprof.Cmdline)
+	s.router.HandleFunc("/debug/pprof/profile", pprof.Profile)
+	s.router.HandleFunc("/debug/pprof/symbol", pprof.Symbol)
+	s.router.HandleFunc("/debug/pprof/trace", pprof.Trace)
 
 	s.router.HandleFunc(fmt.Sprintf("/v1/models/%s:predict", s.options.ModelName), s.PredictHandler).Methods("POST")
 
