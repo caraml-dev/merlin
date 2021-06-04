9044dbb9588e24a93544d6fdda3301dbf8dd7988
Khor Shu Heng <khor.heng@gojek.com>
2021-06-04 13:59:50 +0800

    Prevent dataloss during database migration
    Signed-off-by: Khor Shu Heng <khor.heng@gojek.com>

--- db-migrations/25_message_varchar_length.down.sql
+++ db-migrations/25_message_varchar_length.down.sql
@@ -1,2 +1,1 @@
-ALTER TABLE version_endpoints DROP COLUMN message;
-ALTER TABLE version_endpoints ADD COLUMN message varchar(64);
+ALTER TABLE version_endpoints ALTER COLUMN message varchar(64) USING SUBSTR(message, 1, 64);
