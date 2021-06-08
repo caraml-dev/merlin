002272fb0454818cf33a29d2991729d4c1655304
aria <pradithya.pura@go-jek.com>
2021-06-08 11:35:16 +0800

    Fix regression in image builder test (#145)
    * Add TerminationMessagePolicy in the imagebuilder_test.go
    
    * Fix migration script

--- db-migrations/25_message_varchar_length.down.sql
+++ db-migrations/25_message_varchar_length.down.sql
@@ -1,1 +1,1 @@
-ALTER TABLE version_endpoints ALTER COLUMN message varchar(64) USING SUBSTR(message, 1, 64);
+ALTER TABLE version_endpoints ALTER COLUMN message TYPE varchar(64) USING SUBSTR(message, 1, 64);
