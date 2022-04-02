cc03070673dc25e4aabd6686721dfe0be375ec91
Pradithya Aria <pradithya.pura@go-jek.com>
2022-04-02 19:36:08 +0800

    Only run passing e2e tests

--- scripts/e2e/deploy-merlin.sh
+++ scripts/e2e/deploy-merlin.sh
@@ -9,6 +9,7 @@ INGRESS_HOST="$1"
 DOCKER_REGISTRY="$2"
 CHART_PATH="$3"
 VERSION="$4"
+GIT_REF="$5"
 
 TIMEOUT=120s
 
@@ -32,12 +33,16 @@ install_mlp() {
 install_merlin() {
   echo "::group::Merlin Deployment"
   # Merlin uses vault-secret to connect to vault
-  kubectl create secret generic vault-secret --namespace=mlp --from-literal=address=http://vault.vault.svc.cluster.local --from-literal=token=root
+  kubectl create secret generic vault-secret --namespace=mlp --from-literal=address=http://vault.vault.svc.cluster.local --from-literal=token=root --dry-run=client -o yaml | kubectl apply -f -
 
   helm upgrade --install --debug merlin ${CHART_PATH} --namespace=mlp --create-namespace -f ${CHART_PATH}/values-e2e.yaml \
     --set merlin.image.registry=${DOCKER_REGISTRY} \
     --set merlin.image.tag=${VERSION} \
     --set merlin.transformer.image=${DOCKER_REGISTRY}/merlin-transformer:${VERSION} \
+    --set merlin.imageBuilder.dockerRegistry=${DOCKER_REGISTRY} \
+    --set merlin.imageBuilder.buildContextURI=git://github.com/gojek/merlin.git#${GIT_REF} \
+    --set merlin.imageBuilder.predictionJobBuildContextURI=git://github.com/gojek/merlin.git#${GIT_REF} \
+    --set merlin.imageBuilder.predictionJobBaseImage=${DOCKER_REGISTRY}/merlin-pyspark-base:${VERSION} \
     --set merlin.apiHost=http://merlin.mlp.${INGRESS_HOST}/v1 \
     --set merlin.ingress.host=merlin.mlp.${INGRESS_HOST} \
     --set mlflow.ingress.host=merlin-mlflow.mlp.${INGRESS_HOST} \
