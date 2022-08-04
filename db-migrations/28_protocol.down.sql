1e6cddb1cab808e93fb03e58afea0f83430ed8ce
Pradithya Aria <pradithya.pura@go-jek.com>
2022-08-04 19:19:10 +0800

    Fix db schema
    Signed-off-by: Pradithya Aria <pradithya.pura@go-jek.com>

--- db-migrations/28_protocol.down.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE version_endpoints DROP COLUMN protocol;
-ALTER TABLE model_endpoints DROP COLUMN protocol;
--- db-migrations/29_protocol.up.sql
+++ db-migrations/29_protocol.up.sql
@@ -1,2 +1,2 @@
-ALTER TABLE version_endpoints ADD COLUMN protocol CHAR(32);
-ALTER TABLE model_endpoints ADD COLUMN protocol CHAR(32);
+ALTER TABLE version_endpoints ADD COLUMN protocol VARCHAR(32);
+ALTER TABLE model_endpoints ADD COLUMN protocol VARCHAR(32);
