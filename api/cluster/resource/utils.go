32dc2add86447e28432151351135528b600b511a
Pradithya Aria <pradithya.pura@go-jek.com>
2022-09-27 13:16:28 +0800

    Change autoscaling target value when memory usage is selected as the target metrics
    Signed-off-by: Pradithya Aria <pradithya.pura@go-jek.com>

--- api/cluster/resource/utils.go
+++ api/cluster/resource/utils.go
@@ -2,25 +2,34 @@ package resource
 
 import (
 	"fmt"
+	"math"
 
+	"github.com/gojek/merlin/models"
 	"github.com/gojek/merlin/pkg/autoscaling"
 	"github.com/gojek/merlin/pkg/deployment"
 	kserveconstant "github.com/kserve/kserve/pkg/constants"
+	"k8s.io/apimachinery/pkg/api/resource"
 	knautoscaling "knative.dev/serving/pkg/apis/autoscaling"
 )
 
-func toKNativeAutoscalerMetrics(metricsType autoscaling.MetricsType) (string, error) {
+func toKNativeAutoscalerMetrics(metricsType autoscaling.MetricsType, metricsValue float64, resourceReq *models.ResourceRequest) (string, string, error) {
 	switch metricsType {
 	case autoscaling.CPUUtilization:
-		return knautoscaling.CPU, nil
+		targetValue := fmt.Sprintf("%.0f", metricsValue)
+		return knautoscaling.CPU, targetValue, nil
 	case autoscaling.MemoryUtilization:
-		return knautoscaling.Memory, nil
+		// Calculate memory target value based on memory request * metricsValue
+		// ref: https://github.com/krithika369/turing/blob/6cc3b5f673ccf66e9c1351b2d91382315e86f8ce/api/turing/cluster/knative_service.go#L166
+		memoryTarget := computeResource(resourceReq.MemoryRequest, metricsValue/100)
+		return knautoscaling.Memory, fmt.Sprintf("%.0f", float64(memoryTarget.Value())/math.Pow(1024, 2)), nil
 	case autoscaling.RPS:
-		return knautoscaling.RPS, nil
+		targetValue := fmt.Sprintf("%.0f", metricsValue)
+		return knautoscaling.RPS, targetValue, nil
 	case autoscaling.Concurrency:
-		return knautoscaling.Concurrency, nil
+		targetValue := fmt.Sprintf("%.0f", metricsValue)
+		return knautoscaling.Concurrency, targetValue, nil
 	default:
-		return "", fmt.Errorf("unsuppported autoscaler metrics on serverless deployment: %s", metricsType)
+		return "", "", fmt.Errorf("unsuppported autoscaler metrics on serverless deployment: %s", metricsType)
 	}
 }
 
@@ -42,4 +51,20 @@ func toKServeDeploymentMode(deploymentMode deployment.Mode) (string, error) {
 	default:
 		return "", fmt.Errorf("unsupported deployment mode: %s", deploymentMode)
 	}
+}
+
+func computeResource(resourceQuantity resource.Quantity, fraction float64) resource.Quantity {
+	scaledValue := resourceQuantity.Value()
+
+	scaledMilliValue := int64(math.MaxInt64 - 1)
+	if scaledValue < (math.MaxInt64 / 1000) {
+		scaledMilliValue = resourceQuantity.MilliValue()
+	}
+	percentageValue := float64(scaledMilliValue) * fraction
+	newValue := int64(math.MaxInt64)
+	if percentageValue < math.MaxInt64 {
+		newValue = int64(percentageValue)
+	}
+	newquantity := resource.NewMilliQuantity(newValue, resource.BinarySI)
+	return *newquantity
 }
