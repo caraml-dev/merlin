0d216ed72d3456d8a6000c532008b26948ff35fb
Pradithya Aria <pradithya.pura@go-jek.com>
2022-09-27 14:33:34 +0800

    Add more detailed comments
    Signed-off-by: Pradithya Aria <pradithya.pura@go-jek.com>

--- api/cluster/resource/utils.go
+++ api/cluster/resource/utils.go
@@ -20,7 +20,8 @@ func toKNativeAutoscalerMetrics(metricsType autoscaling.MetricsType, metricsValu
 	case autoscaling.MemoryUtilization:
 		// Calculate memory target value based on memory request * metricsValue
 		// ref: https://github.com/krithika369/turing/blob/6cc3b5f673ccf66e9c1351b2d91382315e86f8ce/api/turing/cluster/knative_service.go#L166
-		memoryTarget := computeResource(resourceReq.MemoryRequest, metricsValue/100)
+		memoryTarget := scaleQuantity(resourceReq.MemoryRequest, metricsValue/100)
+		// Divide value by (1024^2) to convert to Mi
 		return knautoscaling.Memory, fmt.Sprintf("%.0f", float64(memoryTarget.Value())/math.Pow(1024, 2)), nil
 	case autoscaling.RPS:
 		targetValue := fmt.Sprintf("%.0f", metricsValue)
@@ -53,7 +54,9 @@ func toKServeDeploymentMode(deploymentMode deployment.Mode) (string, error) {
 	}
 }
 
-func computeResource(resourceQuantity resource.Quantity, fraction float64) resource.Quantity {
+// ref: https://github.com/knative/serving/blob/release-0.14/pkg/reconciler/revision/resources/queue.go#L115
+// scaleQuantity return fraction value of the resourceQuantity
+func scaleQuantity(resourceQuantity resource.Quantity, fraction float64) resource.Quantity {
 	scaledValue := resourceQuantity.Value()
 
 	scaledMilliValue := int64(math.MaxInt64 - 1)
