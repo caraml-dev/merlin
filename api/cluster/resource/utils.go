11b1f1972251d13cbedfdc3f439ba2810c12b7ad
aria <pradithya.pura@go-jek.com>
2022-09-27 16:39:20 +0800

    Fix autoscaling target value when memory usage is selected as the target metrics (#288)
    **What this PR does / why we need it**:
    <!-- Explain here the context and why you're making the change. What is
    the problem you're trying to solve. --->
    Knative expect the value of `autoscaling.knative.dev/target` annotation
    to be in `MiB` units when `autoscaling.knative.dev/metric` is set to
    memory (i.e. memory-bound autoscaling).
    Existing implementation expect users to provide memory usage target %
    and pass it directly to knative without conversion. It leads to
    incorrect behavior, in which the autoscaling target will only vary
    between 0-100MiB.
    This PR address this bugs by converting the target memory utilization %
    provided by users to absolute memory requested by using user's requested
    memory resource.
    
    
    **Does this PR introduce a user-facing change?**:
    <!--
    If no, just write "NONE" in the release-note block below.
    If yes, a release note is required. Enter your extended release note in
    the block below.
    If the PR requires additional action from users switching to the new
    release, include the string "action required".
    
    For more information about release notes, see kubernetes' guide here:
    http://git.k8s.io/community/contributors/guide/release-notes.md
    -->
    
    ```release-note
    Fix incorrect memory-bound autoscaling behavior
    ```
    
    **Checklist**
    
    - [X] Added unit test, integration, and/or e2e tests
    - [X] Tested locally
    - [N.A.] Updated documentation
    - [N.A.] Update Swagger spec if the PR introduce API changes
    - [N.A.] Regenerated Golang and Python client if the PR introduce API
    changes
    
    Signed-off-by: Pradithya Aria <pradithya.pura@go-jek.com>

--- api/cluster/resource/utils.go
+++ api/cluster/resource/utils.go
@@ -2,25 +2,35 @@ package resource
 
 import (
 	"fmt"
+	"math"
 
+	"github.com/gojek/merlin/models"
 	"github.com/gojek/merlin/pkg/autoscaling"
 	"github.com/gojek/merlin/pkg/deployment"
 	kserveconstant "github.com/kserve/kserve/pkg/constants"
+	"k8s.io/apimachinery/pkg/api/resource"
 	knautoscaling "knative.dev/serving/pkg/apis/autoscaling"
 )
 
-func toKNativeAutoscalerMetrics(metricsType autoscaling.MetricsType) (string, error) {
+func toKNativeAutoscalerMetrics(metricsType autoscaling.MetricsType, metricsValue float64, resourceReq *models.ResourceRequest) (string, string, error) {
 	switch metricsType {
 	case autoscaling.CPUUtilization:
-		return knautoscaling.CPU, nil
+		targetValue := fmt.Sprintf("%.0f", metricsValue)
+		return knautoscaling.CPU, targetValue, nil
 	case autoscaling.MemoryUtilization:
-		return knautoscaling.Memory, nil
+		// Calculate memory target value based on memory request * metricsValue
+		// ref: https://github.com/krithika369/turing/blob/6cc3b5f673ccf66e9c1351b2d91382315e86f8ce/api/turing/cluster/knative_service.go#L166
+		memoryTarget := scaleQuantity(resourceReq.MemoryRequest, metricsValue/100)
+		// Divide value by (1024^2) to convert to Mi
+		return knautoscaling.Memory, fmt.Sprintf("%.0f", float64(memoryTarget.Value())/math.Pow(1024, 2)), nil
 	case autoscaling.RPS:
-		return knautoscaling.RPS, nil
+		targetValue := fmt.Sprintf("%.0f", metricsValue)
+		return knautoscaling.RPS, targetValue, nil
 	case autoscaling.Concurrency:
-		return knautoscaling.Concurrency, nil
+		targetValue := fmt.Sprintf("%.0f", metricsValue)
+		return knautoscaling.Concurrency, targetValue, nil
 	default:
-		return "", fmt.Errorf("unsuppported autoscaler metrics on serverless deployment: %s", metricsType)
+		return "", "", fmt.Errorf("unsuppported autoscaler metrics on serverless deployment: %s", metricsType)
 	}
 }
 
@@ -42,4 +52,22 @@ func toKServeDeploymentMode(deploymentMode deployment.Mode) (string, error) {
 	default:
 		return "", fmt.Errorf("unsupported deployment mode: %s", deploymentMode)
 	}
+}
+
+// ref: https://github.com/knative/serving/blob/release-0.14/pkg/reconciler/revision/resources/queue.go#L115
+// scaleQuantity return fraction value of the resourceQuantity
+func scaleQuantity(resourceQuantity resource.Quantity, fraction float64) resource.Quantity {
+	scaledValue := resourceQuantity.Value()
+
+	scaledMilliValue := int64(math.MaxInt64 - 1)
+	if scaledValue < (math.MaxInt64 / 1000) {
+		scaledMilliValue = resourceQuantity.MilliValue()
+	}
+	percentageValue := float64(scaledMilliValue) * fraction
+	newValue := int64(math.MaxInt64)
+	if percentageValue < math.MaxInt64 {
+		newValue = int64(percentageValue)
+	}
+	newquantity := resource.NewMilliQuantity(newValue, resource.BinarySI)
+	return *newquantity
 }
