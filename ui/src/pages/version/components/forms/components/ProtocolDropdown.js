46e63ec2bc04ac226042e2d0cad7534a768366bf
aria <pradithya.pura@go-jek.com>
2022-09-28 13:27:07 +0800

    UI implementation for UPI  (#287)
    <!--  Thanks for sending a pull request!  Here are some tips for you:
    
    1. Run unit tests and ensure that they are passing
    2. If your change introduces any API changes, make sure to update the
    e2e tests
    3. Make sure documentation is updated for your PR!
    
    -->
    
    **What this PR does / why we need it**:
    <!-- Explain here the context and why you're making the change. What is
    the problem you're trying to solve. --->
    This PR mainly expose `protocol` configuration when deploying a model
    version. The main modifications are in 2 places:
    
    ### During deployment of new model version
    `protocol` will be configurable when users deploy a new model version.
    The protocol selection is only available if the model version endpoint
    is not in serving state.
    
    <img width="1027" alt="Screenshot of the protocol selection."
    src="https://user-images.githubusercontent.com/4023015/192190456-b821cfd3-0c8e-4140-be3d-9d02c8e9b55b.png">
    
    
    https://user-images.githubusercontent.com/4023015/192190512-8f77b945-6c07-4c84-9728-730d1c109d5c.mov
    
    ### Opening the endpoint details 
    
    Currently selected protocol will be exposed in the version endpoint
    details page additionally, the endpoint URL is also adjusted based on
    the selected protocol.
    
    <img width="1405" alt="Screenshot 2022-09-26 at 11 57 38 AM"
    src="https://user-images.githubusercontent.com/4023015/192191248-ad44fa81-233b-49cc-bd00-f30d570c4de1.png">
    <img width="1430" alt="Screenshot 2022-09-26 at 11 56 28 AM"
    src="https://user-images.githubusercontent.com/4023015/192191263-40843c91-8519-4993-ad40-7d957c5aa913.png">
    
    
    **Which issue(s) this PR fixes**:
    <!--
    *Automatically closes linked issue when PR is merged.
    Usage: `Fixes #<issue number>`, or `Fixes (paste link of issue)`.
    -->
    
    Fixes #
    
    **Does this PR introduce a user-facing change?**:
    <!--
    If no, just write "NONE" in the release-note block below.
    If yes, a release note is required. Enter your extended release note in
    the block below.
    If the PR requires additional action from users switching to the new
    release, include the string "action required".
    
    For more information about release notes, see kubernetes' guide here:
    http://git.k8s.io/community/contributors/guide/release-notes.md
    -->
    
    ```release-note
    Expose protocol selection to user
    ```
    
    **Checklist**
    
    - [N.A.] Added unit test, integration, and/or e2e tests
    - [X] Tested locally
    - [N.A.] Updated documentation
    - [N.A.] Update Swagger spec if the PR introduce API changes
    - [N.A.] Regenerated Golang and Python client if the PR introduce API
    changes
    
    Co-authored-by: Alvin Lee <alvin.lee@gojek.com>

---
+++ ui/src/pages/version/components/forms/components/ProtocolDropdown.js
@@ -0,0 +1,65 @@
+import React, { Fragment } from "react";
+import { EuiSuperSelect, EuiText } from "@elastic/eui";
+
+/**
+ * Dropdown menu for selecting protocol
+ *
+ * @param {*} endpointStatus current endpoint status
+ * @param {*} protocol current selected protocol
+ * @param {*} onProtocolChange callback to be made when protocol selection changed
+ *
+ */
+export const ProtocolDropdown = ({
+  endpointStatus,
+  protocol,
+  onProtocolChange,
+}) => {
+  // Users should not be able to change protocol when the model endpoint status is serving
+  const isDisabled = (endpointStatus) => {
+    return endpointStatus === "serving";
+  };
+
+  const onChange = (value) => {
+    onProtocolChange(value);
+  };
+
+  return (
+    <EuiSuperSelect
+      fullWidth
+      options={[
+        {
+          value: "HTTP_JSON",
+          inputDisplay: "JSON over HTTP (REST)",
+          dropdownDisplay: (
+            <Fragment>
+              <strong>JSON over HTTP (REST)</strong>
+              <EuiText size="s" color="subdued">
+                <p className="euiTextColor--subdued">
+                  (Default) Expose model as HTTP service
+                </p>
+              </EuiText>
+            </Fragment>
+          ),
+        },
+        {
+          value: "UPI_V1",
+          inputDisplay: "Universal Prediction Interface (gRPC)",
+          dropdownDisplay: (
+            <Fragment>
+              <strong>Universal Prediction Interface (gRPC)</strong>
+              <EuiText size="s" color="subdued">
+                <p className="euiTextColor--subdued">
+                  Expose model as gRPC service compatible with UPI
+                </p>
+              </EuiText>
+            </Fragment>
+          ),
+        },
+      ]}
+      valueOfSelected={protocol}
+      onChange={onChange}
+      disabled={isDisabled(endpointStatus)}
+      hasDividers
+    />
+  );
+};
