380fe980acc2fda950f695467ab36073f9cc39bc
Alvin Lee <alvin.lee@gojek.com>
2022-10-09 03:55:32 +0800

    generate openapi files

--- python/sdk/client/api/__init__.py
+++ python/sdk/client/api/__init__.py
@@ -3,14 +3,14 @@ from __future__ import absolute_import
 # flake8: noqa
 
 # import apis into api package
-from client.api.alert_api import AlertApi
-from client.api.endpoint_api import EndpointApi
-from client.api.environment_api import EnvironmentApi
-from client.api.log_api import LogApi
-from client.api.model_endpoints_api import ModelEndpointsApi
-from client.api.models_api import ModelsApi
-from client.api.prediction_jobs_api import PredictionJobsApi
-from client.api.project_api import ProjectApi
-from client.api.secret_api import SecretApi
-from client.api.standard_transformer_api import StandardTransformerApi
-from client.api.version_api import VersionApi
+from swagger_client.api.alert_api import AlertApi
+from swagger_client.api.endpoint_api import EndpointApi
+from swagger_client.api.environment_api import EnvironmentApi
+from swagger_client.api.log_api import LogApi
+from swagger_client.api.model_endpoints_api import ModelEndpointsApi
+from swagger_client.api.models_api import ModelsApi
+from swagger_client.api.prediction_jobs_api import PredictionJobsApi
+from swagger_client.api.project_api import ProjectApi
+from swagger_client.api.secret_api import SecretApi
+from swagger_client.api.standard_transformer_api import StandardTransformerApi
+from swagger_client.api.version_api import VersionApi
