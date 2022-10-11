52a3b14938cf0858cf468427abe2d43114e6598c
Alvin Lee <alvin.lee@gojek.com>
2022-10-11 14:47:30 +0800

    fix helm command in ci

--- scripts/e2e/deploy-merlin.sh
+++ scripts/e2e/deploy-merlin.sh
@@ -45,10 +45,10 @@ install_merlin() {
     --set merlin.imageBuilder.predictionJobBuildContextURI=git://github.com/gojek/merlin.git#${GIT_REF} \
     --set merlin.imageBuilder.predictionJobBaseImage=\
           '{ \
-            "3.7.*": ${DOCKER_REGISTRY}/merlin/merlin-pyspark-base-py37:${VERSION}, \
-            "3.8.*": ${DOCKER_REGISTRY}/merlin/merlin-pyspark-base-py38:${VERSION}, \
-            "3.9.*": ${DOCKER_REGISTRY}/merlin/merlin-pyspark-base-py39:${VERSION}, \
-            "3.10.*": ${DOCKER_REGISTRY}/merlin/merlin-pyspark-base-py310:${VERSION}, \
+            \"3.7.*\": ${DOCKER_REGISTRY}/merlin/merlin-pyspark-base-py37:${VERSION}, \
+            \"3.8.*\": ${DOCKER_REGISTRY}/merlin/merlin-pyspark-base-py38:${VERSION}, \
+            \"3.9.*\": ${DOCKER_REGISTRY}/merlin/merlin-pyspark-base-py39:${VERSION}, \
+            \"3.10.*\": ${DOCKER_REGISTRY}/merlin/merlin-pyspark-base-py310:${VERSION}, \
           }' \
     --set merlin.apiHost=http://merlin.mlp.${INGRESS_HOST}/v1 \
     --set merlin.ingress.host=merlin.mlp.${INGRESS_HOST} \
