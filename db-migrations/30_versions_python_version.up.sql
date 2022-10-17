7c580f11bf158d5de6cdf6d008dabcafb8f96099
Alvin Lee <alvin.lee@gojek.com>
2022-10-17 17:56:38 +0800

    remove  from sql causing error

--- db-migrations/30_versions_python_version.up.sql
+++ db-migrations/30_versions_python_version.up.sql
@@ -1,5 +1,3 @@
 -- Introduce Python version column, with default as version 3.7 which is the
 -- only supported major version as of the introduction of the column.
-ALTER TABLE versions ADD COLUMN python_version varchar(16) NOT NULL DEFAULT '3.7.*' WITH VALUES;
--- Drop default after existing rows are updated
-ALTER TABLE versions ALTER COLUMN python_version DROP DEFAULT;
+ALTER TABLE versions ADD COLUMN python_version varchar(16) NOT NULL DEFAULT '3.7.*';
