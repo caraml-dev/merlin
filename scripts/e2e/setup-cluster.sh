31a944093ccc750794241af7a6a17dba37f16e97
shydefoo <shidefoo@gmail.com>
2023-01-30 13:24:02 +0800

    Modify merlin workflow to use k8sconfig

--- scripts/e2e/setup-cluster.sh
+++ scripts/e2e/setup-cluster.sh
@@ -6,7 +6,7 @@ set -o pipefail
 set -o nounset
 
 # Software requirements:
-# - yq 4.24.2 
+# - yq 4.24.2
 # - helm 3
 # - k3d
 # - kubectl
@@ -22,74 +22,60 @@ ISTIO_VERSION=1.13.2
 KNATIVE_VERSION=1.7.4
 KNATIVE_NET_ISTIO_VERSION=1.7.1
 CERT_MANAGER_VERSION=1.7.2
-MINIO_VERSION=3.6.3 
+MINIO_VERSION=3.6.3
 KSERVE_VERSION=0.8.0
-VAULT_VERSION=0.19.0
+# VAULT_VERSION=0.19.0
 TIMEOUT=180s
 
-
 add_helm_repo() {
-    helm repo add hashicorp https://helm.releases.hashicorp.com
-    helm repo add minio https://charts.min.io/
-    helm repo add istio https://istio-release.storage.googleapis.com/charts
-    helm repo update
-}
-
-install_vault() {
-    echo "::group::Vault Deployment"
-    helm upgrade --install vault hashicorp/vault --version=${VAULT_VERSION} \
-        -f config/vault/values.yaml \
-        --namespace=vault --create-namespace \
-        --wait --timeout=${TIMEOUT}
-
-    kubectl wait pod/vault-0 --namespace=vault --for=condition=ready --timeout=${TIMEOUT}
+	helm repo add hashicorp https://helm.releases.hashicorp.com
+	helm repo add minio https://charts.min.io/
+	helm repo add istio https://istio-release.storage.googleapis.com/charts
+	helm repo update
 }
 
 store_cluster_secret() {
-      echo "::group::Storing Cluster Secret"
-    # Downgrade to Vault KV secrets engine version 1
-    kubectl exec vault-0 --namespace=vault -- vault secrets disable secret
-    kubectl exec vault-0 --namespace=vault -- vault secrets enable -version=1 -path=secret kv
+	echo "::group::Storing Cluster Secret"
 
-    k3d kubeconfig get $CLUSTER_NAME > kubeconfig.yaml
-
-    # Write cluster credential to be saved in Vault
-    cat <<EOF > cluster-credential.json
+	k3d kubeconfig get default >/tmp/temp_kubeconfig.yaml
+	cat <<EOF >/tmp/temp_k8sconfig.json
 {
-"name": "$(yq '.clusters[0].name' kubeconfig.yaml)",
-"master_ip": "kubernetes.default:443",
-"certs": "$(yq '.clusters[0].cluster."certificate-authority-data"' kubeconfig.yaml | base64 --decode | awk '{printf "%s\\n", $0}')",
-"client_certificate": "$(yq '.users[0].user."client-certificate-data"' kubeconfig.yaml | base64 --decode | awk '{printf "%s\\n", $0}')",
-"client_key": "$(yq '.users[0].user."client-key-data"' kubeconfig.yaml | base64 --decode | awk '{printf "%s\\n", $0}')"
+    "k8sConfig": {
+        "name": $(yq .clusters[0].name -o json /tmp/temp_kubeconfig.yaml),
+        "cluster": $(yq .clusters[0].cluster -o json /tmp/temp_kubeconfig.yaml),
+        "user": $(yq .users[0].user -o json /tmp/temp_kubeconfig.yaml)
+    }
 }
 EOF
+	# get yaml file
+	yq e -P /tmp/temp_k8sconfig.json -o yaml >/tmp/temp_k8sconfig.yaml
 
-    # Save KinD cluster credential to Vault
-    kubectl cp cluster-credential.json vault/vault-0:/tmp/cluster-credential.json
-    kubectl exec vault-0 --namespace=vault -- vault kv put secret/${CLUSTER_NAME} @/tmp/cluster-credential.json
+	# use yaml file
+	# yq '.[0] *= load("/tmp/temp_k8sconfig.yaml")' rest_config.yaml
 
-    # Clean created credential files
-    rm kubeconfig.yaml
-    rm cluster-credential.json
+	# NOTE: Do no delete files created here as they will be used in the next step
+	# TODO: Figure out a better way to pass cluster K8sConfig
+	# rm /tmp/temp_kubeconfig.yaml
+	# rm /tmp/temp_k8sconfig.yaml
 }
 
 install_istio() {
-    echo "::group::Istio Deployment"
-    helm upgrade --install istio-base istio/base --version=${ISTIO_VERSION} -n istio-system --create-namespace
-    helm upgrade --install istiod istio/istiod --version=${ISTIO_VERSION} -n istio-system --create-namespace \
-        -f config/istio/istiod.yaml --timeout=${TIMEOUT}
-    
-    helm upgrade --install istio-ingressgateway istio/gateway -n istio-system --create-namespace \
-        -f config/istio/ingress-gateway.yaml --timeout=${TIMEOUT}
-    
-    helm upgrade --install cluster-local-gateway istio/gateway -n istio-system --create-namespace \
-        -f config/istio/clusterlocal-gateway.yaml --timeout=${TIMEOUT}
+	echo "::group::Istio Deployment"
+	helm upgrade --install istio-base istio/base --version=${ISTIO_VERSION} -n istio-system --create-namespace
+	helm upgrade --install istiod istio/istiod --version=${ISTIO_VERSION} -n istio-system --create-namespace \
+		-f config/istio/istiod.yaml --timeout=${TIMEOUT}
 
-    kubectl rollout status deployment/istio-ingressgateway -n istio-system -w --timeout=${TIMEOUT}
-    kubectl rollout status deployment/istiod -w -n istio-system --timeout=${TIMEOUT}
-    kubectl rollout status deployment/cluster-local-gateway -n istio-system -w --timeout=${TIMEOUT}
+	helm upgrade --install istio-ingressgateway istio/gateway -n istio-system --create-namespace \
+		-f config/istio/ingress-gateway.yaml --timeout=${TIMEOUT}
 
-    kubectl apply -f config/istio/ingress-class.yaml
+	helm upgrade --install cluster-local-gateway istio/gateway -n istio-system --create-namespace \
+		-f config/istio/clusterlocal-gateway.yaml --timeout=${TIMEOUT}
+
+	kubectl rollout status deployment/istio-ingressgateway -n istio-system -w --timeout=${TIMEOUT}
+	kubectl rollout status deployment/istiod -w -n istio-system --timeout=${TIMEOUT}
+	kubectl rollout status deployment/cluster-local-gateway -n istio-system -w --timeout=${TIMEOUT}
+
+	kubectl apply -f config/istio/ingress-class.yaml
 }
 
 install_knative() {
@@ -116,37 +102,37 @@ install_knative() {
 }
 
 install_cert_manager() {
-    echo "::group::Cert Manager Deployment"
-    kubectl apply --filename=https://github.com/jetstack/cert-manager/releases/download/v${CERT_MANAGER_VERSION}/cert-manager.yaml
+	echo "::group::Cert Manager Deployment"
+	kubectl apply --filename=https://github.com/jetstack/cert-manager/releases/download/v${CERT_MANAGER_VERSION}/cert-manager.yaml
 
-    kubectl rollout status deployment/cert-manager-webhook -n cert-manager -w --timeout=${TIMEOUT}
-    kubectl rollout status deployment/cert-manager-cainjector -n cert-manager -w --timeout=${TIMEOUT}
-    kubectl rollout status deployment/cert-manager -n cert-manager -w --timeout=${TIMEOUT}
+	kubectl rollout status deployment/cert-manager-webhook -n cert-manager -w --timeout=${TIMEOUT}
+	kubectl rollout status deployment/cert-manager-cainjector -n cert-manager -w --timeout=${TIMEOUT}
+	kubectl rollout status deployment/cert-manager -n cert-manager -w --timeout=${TIMEOUT}
 }
 
 install_minio() {
-    echo "::group::Minio Deployment"
-    helm upgrade --install minio minio/minio --version=${MINIO_VERSION} -f config/minio/values.yaml \
-        --namespace=minio --create-namespace --timeout=${TIMEOUT} \
-        --set accessKey=YOURACCESSKEY --set secretKey=YOURSECRETKEY \
-        --set "ingress.hosts[0]=minio.minio.${INGRESS_HOST}" \
-        --set "consoleIngress.hosts[0]=console.minio.${INGRESS_HOST}"
+	echo "::group::Minio Deployment"
+	helm upgrade --install minio minio/minio --version=${MINIO_VERSION} -f config/minio/values.yaml \
+		--namespace=minio --create-namespace --timeout=${TIMEOUT} \
+		--set accessKey=YOURACCESSKEY --set secretKey=YOURSECRETKEY \
+		--set "ingress.hosts[0]=minio.minio.${INGRESS_HOST}" \
+		--set "consoleIngress.hosts[0]=console.minio.${INGRESS_HOST}"
 
-    kubectl rollout status statefulset/minio -n minio -w --timeout=${TIMEOUT}
+	kubectl rollout status statefulset/minio -n minio -w --timeout=${TIMEOUT}
 }
 
 install_kserve() {
-    echo "::group::KServe Deployment"
-    wget https://raw.githubusercontent.com/kserve/kserve/master/install/v${KSERVE_VERSION}/kserve.yaml -O config/kserve/kserve.yaml
-    kubectl apply -k config/kserve
-    kubectl rollout status statefulset/kserve-controller-manager -n kserve -w --timeout=${TIMEOUT}
-    kubectl apply -f https://raw.githubusercontent.com/kserve/kserve/master/install/v${KSERVE_VERSION}/kserve-runtimes.yaml
+	echo "::group::KServe Deployment"
+	wget https://raw.githubusercontent.com/kserve/kserve/master/install/v${KSERVE_VERSION}/kserve.yaml -O config/kserve/kserve.yaml
+	kubectl apply -k config/kserve
+	kubectl rollout status statefulset/kserve-controller-manager -n kserve -w --timeout=${TIMEOUT}
+	kubectl apply -f https://raw.githubusercontent.com/kserve/kserve/master/install/v${KSERVE_VERSION}/kserve-runtimes.yaml
 }
 
 patch_coredns() {
-    echo "::group::Patching CoreDNS"
-    kubectl patch cm coredns -n kube-system --patch-file config/coredns/patch.yaml 
-    kubectl get cm coredns -n kube-system -o yaml
+	echo "::group::Patching CoreDNS"
+	kubectl patch cm coredns -n kube-system --patch-file config/coredns/patch.yaml
+	kubectl get cm coredns -n kube-system -o yaml
 }
 
 add_helm_repo
