c87b1181b7f1f7de6bbe613b14bbefdda011a1a9
shydefoo <shidefoo@gmail.com>
2023-02-23 14:16:54 +0800

    Remove changes to e2e test

--- scripts/e2e/run-e2e.sh
+++ scripts/e2e/run-e2e.sh
@@ -32,6 +32,5 @@ kubectl create namespace ${E2E_PROJECT_NAME} --dry-run=client -o yaml | kubectl 
 
 cd ../../python/sdk
 pip install pipenv==2022.8.19
-pip install --upgrade pip
 pipenv install --dev --skip-lock --python ${PYTHON_VERSION}
 pipenv run pytest -n=8 -W=ignore --cov=merlin -m "not (feast or batch or pyfunc or local_server_test or cli or customtransformer)"
