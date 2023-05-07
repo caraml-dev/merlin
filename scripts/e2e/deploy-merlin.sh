30897bebe694764cd8dcebc445bfc1c80cc81db0
Arief Rahmansyah <ariefrahmansyah@hotmail.com>
2023-05-08 03:06:09 +0700

    Update e2e test scripts & ci

--- scripts/e2e/deploy-merlin.sh
+++ scripts/e2e/deploy-merlin.sh
@@ -7,17 +7,15 @@ set -o nounset
 
 INGRESS_HOST="$1"
 DOCKER_REGISTRY="$2"
-CHART_PATH="$3"
-VERSION="$4"
-GIT_REF="$5"
-MLP_CHART_VERSION="$6"
+VERSION="$3"
+GIT_REF="$4"
+MLP_CHART_VERSION="$5"
+MERLIN_CHART_VERSION="$6"
 
 TIMEOUT=300s
 
 install_mlp() {
   echo "::group::MLP Deployment"
-
-  helm repo add caraml https://caraml-dev.github.io/helm-charts
 
   helm upgrade --install --debug mlp caraml/mlp --namespace mlp --create-namespace \
     --version ${MLP_CHART_VERSION} \
@@ -38,36 +36,39 @@ install_merlin() {
     echo "::group::Merlin Deployment"
 # build in json first, then convert to yaml
   output=$(yq e -o json '.k8s_config' /tmp/temp_k8sconfig.yaml | jq -r -M -c .)
-  output="$output" yq '.merlin.environmentConfigs[0] *= load("/tmp/temp_k8sconfig.yaml") | .merlin.imageBuilder.k8sConfig |= strenv(output)' -i "${CHART_PATH}/values-e2e.yaml"
+  output="$output" yq '.environmentConfigs[0] *= load("/tmp/temp_k8sconfig.yaml") | .imageBuilder.k8sConfig |= strenv(output)' -i "./values-e2e.yaml"
 
-  helm upgrade --install --debug merlin ${CHART_PATH} --namespace=mlp --create-namespace -f ${CHART_PATH}/values-e2e.yaml \
-    --set merlin.image.registry=${DOCKER_REGISTRY} \
-    --set merlin.image.tag=${VERSION} \
-    --set merlin.transformer.image=${DOCKER_REGISTRY}/merlin-transformer:${VERSION} \
-    --set merlin.imageBuilder.dockerRegistry=${DOCKER_REGISTRY} \
-    --set merlin.imageBuilder.predictionJobBaseImages."3\.7\.*".imageName=${DOCKER_REGISTRY}/merlin/merlin-pyspark-base-py37:${VERSION} \
-    --set merlin.imageBuilder.predictionJobBaseImages."3\.7\.*".buildContextURI=git://github.com/caraml-dev/merlin.git#${GIT_REF} \
-    --set merlin.imageBuilder.predictionJobBaseImages."3\.7\.*".dockerfilePath=docker/app.Dockerfile \
-    --set merlin.imageBuilder.predictionJobBaseImages."3\.7\.*".mainAppPath=/home/spark/merlin-spark-app/main.py \
-    --set merlin.imageBuilder.predictionJobBaseImages."3\.8\.*".imageName=${DOCKER_REGISTRY}/merlin/merlin-pyspark-base-py38:${VERSION} \
-    --set merlin.imageBuilder.predictionJobBaseImages."3\.8\.*".buildContextURI=git://github.com/caraml-dev/merlin.git#${GIT_REF} \
-    --set merlin.imageBuilder.predictionJobBaseImages."3\.8\.*".dockerfilePath=docker/app.Dockerfile \
-    --set merlin.imageBuilder.predictionJobBaseImages."3\.8\.*".mainAppPath=/home/spark/merlin-spark-app/main.py \
-    --set merlin.imageBuilder.predictionJobBaseImages."3\.9\.*".imageName=${DOCKER_REGISTRY}/merlin/merlin-pyspark-base-py39:${VERSION} \
-    --set merlin.imageBuilder.predictionJobBaseImages."3\.9\.*".buildContextURI=git://github.com/caraml-dev/merlin.git#${GIT_REF} \
-    --set merlin.imageBuilder.predictionJobBaseImages."3\.9\.*".dockerfilePath=docker/app.Dockerfile \
-    --set merlin.imageBuilder.predictionJobBaseImages."3\.9\.*".mainAppPath=/home/spark/merlin-spark-app/main.py \
-    --set merlin.imageBuilder.predictionJobBaseImages."3\.10\.*".imageName=${DOCKER_REGISTRY}/merlin/merlin-pyspark-base-py310:${VERSION} \
-    --set merlin.imageBuilder.predictionJobBaseImages."3\.10\.*".buildContextURI=git://github.com/caraml-dev/merlin.git#${GIT_REF} \
-    --set merlin.imageBuilder.predictionJobBaseImages."3\.10\.*".dockerfilePath=docker/app.Dockerfile \
-    --set merlin.imageBuilder.predictionJobBaseImages."3\.10\.*".mainAppPath=/home/spark/merlin-spark-app/main.py \
-    --set merlin.apiHost=http://merlin.mlp.${INGRESS_HOST}/v1 \
-    --set merlin.ingress.host=merlin.mlp.${INGRESS_HOST} \
+  helm upgrade --install --debug merlin caraml/merlin --namespace=mlp --create-namespace \
+    --version ${MERLIN_CHART_VERSION} \
+    -f values-e2e.yaml \
+    --set deployment.image.registry=${DOCKER_REGISTRY} \
+    --set deployment.image.tag=${VERSION} \
+    --set transformer.image=${DOCKER_REGISTRY}/merlin-transformer:${VERSION} \
+    --set imageBuilder.dockerRegistry=${DOCKER_REGISTRY} \
+    --set imageBuilder.predictionJobBaseImages."3\.7\.*".imageName=${DOCKER_REGISTRY}/merlin/merlin-pyspark-base-py37:${VERSION} \
+    --set imageBuilder.predictionJobBaseImages."3\.7\.*".buildContextURI=git://github.com/caraml-dev/merlin.git#${GIT_REF} \
+    --set imageBuilder.predictionJobBaseImages."3\.7\.*".dockerfilePath=docker/app.Dockerfile \
+    --set imageBuilder.predictionJobBaseImages."3\.7\.*".mainAppPath=/home/spark/merlin-spark-app/main.py \
+    --set imageBuilder.predictionJobBaseImages."3\.8\.*".imageName=${DOCKER_REGISTRY}/merlin/merlin-pyspark-base-py38:${VERSION} \
+    --set imageBuilder.predictionJobBaseImages."3\.8\.*".buildContextURI=git://github.com/caraml-dev/merlin.git#${GIT_REF} \
+    --set imageBuilder.predictionJobBaseImages."3\.8\.*".dockerfilePath=docker/app.Dockerfile \
+    --set imageBuilder.predictionJobBaseImages."3\.8\.*".mainAppPath=/home/spark/merlin-spark-app/main.py \
+    --set imageBuilder.predictionJobBaseImages."3\.9\.*".imageName=${DOCKER_REGISTRY}/merlin/merlin-pyspark-base-py39:${VERSION} \
+    --set imageBuilder.predictionJobBaseImages."3\.9\.*".buildContextURI=git://github.com/caraml-dev/merlin.git#${GIT_REF} \
+    --set imageBuilder.predictionJobBaseImages."3\.9\.*".dockerfilePath=docker/app.Dockerfile \
+    --set imageBuilder.predictionJobBaseImages."3\.9\.*".mainAppPath=/home/spark/merlin-spark-app/main.py \
+    --set imageBuilder.predictionJobBaseImages."3\.10\.*".imageName=${DOCKER_REGISTRY}/merlin/merlin-pyspark-base-py310:${VERSION} \
+    --set imageBuilder.predictionJobBaseImages."3\.10\.*".buildContextURI=git://github.com/caraml-dev/merlin.git#${GIT_REF} \
+    --set imageBuilder.predictionJobBaseImages."3\.10\.*".dockerfilePath=docker/app.Dockerfile \
+    --set imageBuilder.predictionJobBaseImages."3\.10\.*".mainAppPath=/home/spark/merlin-spark-app/main.py \
+    --set ingress.host=merlin.mlp.${INGRESS_HOST} \
     --set mlflow.ingress.host=merlin-mlflow.mlp.${INGRESS_HOST} \
     --wait --timeout=${TIMEOUT}
 
   kubectl rollout status deployment/merlin -n mlp -w --timeout=${TIMEOUT}
 }
+
+helm repo add caraml https://caraml-dev.github.io/helm-charts
 
 install_mlp
 install_merlin
