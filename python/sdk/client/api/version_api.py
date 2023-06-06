0c6b54baddb5f2193feddaef752ce52828cfba2d
Alexander <alexander.i.interns@aux.gojek.com>
2023-06-06 13:37:34 +0700

    feat: generated client from swagger

--- python/sdk/client/api/version_api.py
+++ python/sdk/client/api/version_api.py
@@ -238,6 +238,107 @@ class VersionApi(object):
             _request_timeout=params.get('_request_timeout'),
             collection_formats=collection_formats)
 
+    def models_model_id_versions_version_id_delete(self, model_id, version_id, **kwargs):  # noqa: E501
+        """Delete version by ID from model  # noqa: E501
+
+        This method makes a synchronous HTTP request by default. To make an
+        asynchronous HTTP request, please pass async_req=True
+        >>> thread = api.models_model_id_versions_version_id_delete(model_id, version_id, async_req=True)
+        >>> result = thread.get()
+
+        :param async_req bool
+        :param int model_id: (required)
+        :param int version_id: (required)
+        :return: int
+                 If the method is called asynchronously,
+                 returns the request thread.
+        """
+        kwargs['_return_http_data_only'] = True
+        if kwargs.get('async_req'):
+            return self.models_model_id_versions_version_id_delete_with_http_info(model_id, version_id, **kwargs)  # noqa: E501
+        else:
+            (data) = self.models_model_id_versions_version_id_delete_with_http_info(model_id, version_id, **kwargs)  # noqa: E501
+            return data
+
+    def models_model_id_versions_version_id_delete_with_http_info(self, model_id, version_id, **kwargs):  # noqa: E501
+        """Delete version by ID from model  # noqa: E501
+
+        This method makes a synchronous HTTP request by default. To make an
+        asynchronous HTTP request, please pass async_req=True
+        >>> thread = api.models_model_id_versions_version_id_delete_with_http_info(model_id, version_id, async_req=True)
+        >>> result = thread.get()
+
+        :param async_req bool
+        :param int model_id: (required)
+        :param int version_id: (required)
+        :return: int
+                 If the method is called asynchronously,
+                 returns the request thread.
+        """
+
+        all_params = ['model_id', 'version_id']  # noqa: E501
+        all_params.append('async_req')
+        all_params.append('_return_http_data_only')
+        all_params.append('_preload_content')
+        all_params.append('_request_timeout')
+
+        params = locals()
+        for key, val in six.iteritems(params['kwargs']):
+            if key not in all_params:
+                raise TypeError(
+                    "Got an unexpected keyword argument '%s'"
+                    " to method models_model_id_versions_version_id_delete" % key
+                )
+            params[key] = val
+        del params['kwargs']
+        # verify the required parameter 'model_id' is set
+        if ('model_id' not in params or
+                params['model_id'] is None):
+            raise ValueError("Missing the required parameter `model_id` when calling `models_model_id_versions_version_id_delete`")  # noqa: E501
+        # verify the required parameter 'version_id' is set
+        if ('version_id' not in params or
+                params['version_id'] is None):
+            raise ValueError("Missing the required parameter `version_id` when calling `models_model_id_versions_version_id_delete`")  # noqa: E501
+
+        collection_formats = {}
+
+        path_params = {}
+        if 'model_id' in params:
+            path_params['model_id'] = params['model_id']  # noqa: E501
+        if 'version_id' in params:
+            path_params['version_id'] = params['version_id']  # noqa: E501
+
+        query_params = []
+
+        header_params = {}
+
+        form_params = []
+        local_var_files = {}
+
+        body_params = None
+        # HTTP header `Accept`
+        header_params['Accept'] = self.api_client.select_header_accept(
+            ['*/*'])  # noqa: E501
+
+        # Authentication setting
+        auth_settings = ['Bearer']  # noqa: E501
+
+        return self.api_client.call_api(
+            '/models/{model_id}/versions/{version_id}', 'DELETE',
+            path_params,
+            query_params,
+            header_params,
+            body=body_params,
+            post_params=form_params,
+            files=local_var_files,
+            response_type='int',  # noqa: E501
+            auth_settings=auth_settings,
+            async_req=params.get('async_req'),
+            _return_http_data_only=params.get('_return_http_data_only'),
+            _preload_content=params.get('_preload_content', True),
+            _request_timeout=params.get('_request_timeout'),
+            collection_formats=collection_formats)
+
     def models_model_id_versions_version_id_get(self, model_id, version_id, **kwargs):  # noqa: E501
         """Get version by ID from model  # noqa: E501
 
