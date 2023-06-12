bfd50e0165276876e57e0142335351551a0766d5
Krithika Sundararajan <krithika369@live.com>
2023-06-12 08:58:22 +0800

    feat: Add caching for MLP projects (#405)
    <!--  Thanks for sending a pull request!  Here are some tips for you:
    
    1. Run unit tests and ensure that they are passing
    2. If your change introduces any API changes, make sure to update the
    e2e tests
    3. Make sure documentation is updated for your PR!
    
    -->
    
    **What this PR does / why we need it**:
    <!-- Explain here the context and why you're making the change. What is
    the problem you're trying to solve. --->
    
    This PR introduces a read-aside in-memory cache for the MLP projects
    API, specifically, for getting a project by ID. The implementation is
    largely the same as in
    [Turing](https://github.com/caraml-dev/turing/blob/main/api/turing/service/mlp_service.go),
    with some existing minor differences:
    * The Merlin codebase relies on an [internal MLP
    client](https://github.com/caraml-dev/merlin/blob/main/api/mlp/client.go)
    instead of directly using the one exposed by MLP. This is retained.
    * Turing uses `MLPService` to cover both projects and secrets MLP APIs
    (in addition to Merlin's environments API). In Merlin's codebase this is
    split into the `ProjectsService` and `SecretService`.
    
    **Which issue(s) this PR fixes**:
    <!--
    *Automatically closes linked issue when PR is merged.
    Usage: `Fixes #<issue number>`, or `Fixes (paste link of issue)`.
    -->
    
    With this change, in most cases, look up of individual projects can be
    done in-memory, bypassing the MLP API server. The MLP List projects API
    will only be called under the following scenarios:
    * When Merlin's List projects API is called - this will get the full
    list of projects from the MLP API and (re-)cache them in-memory
    * When a specific project look up in the cache fails, the full list of
    projects will be retrieved from MLP again and re-cached
    
    **Does this PR introduce a user-facing change?**:
    <!--
    If no, just write "NONE" in the release-note block below.
    If yes, a release note is required. Enter your extended release note in
    the block below.
    If the PR requires additional action from users switching to the new
    release, include the string "action required".
    
    For more information about release notes, see kubernetes' guide here:
    http://git.k8s.io/community/contributors/guide/release-notes.md
    -->
    
    ```release-note
    Cache MLP projects for faster responses.
    ```
    
    **Checklist**
    
    - [x] Added unit test, integration, and/or e2e tests
    - [x] Tested locally
    - [ ] Updated documentation
    - [ ] Update Swagger spec if the PR introduce API changes
    - [ ] Regenerated Golang and Python client if the PR introduce API
    changes

--- api/service/project_service.go
+++ api/service/project_service.go
@@ -1,49 +1,142 @@
-// Copyright 2020 The Merlin Authors
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//      http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
 package service
 
 import (
 	"context"
+	"encoding/json"
+	"fmt"
+	"strings"
+	"time"
 
-	"github.com/caraml-dev/merlin/mlp"
+	"github.com/coocood/freecache"
+
+	mlp "github.com/caraml-dev/merlin/mlp"
+	mlpClient "github.com/caraml-dev/mlp/api/client"
 )
 
-type ProjectsService interface {
-	List(ctx context.Context, projectName string) (mlp.Projects, error)
-	GetByID(ctx context.Context, projectID int32) (mlp.Project, error)
-	GetByName(ctx context.Context, projectName string) (mlp.Project, error)
-}
+const (
+	projectCacheExpirySeconds = 600
+	projectCacheSizeBytes     = 1024 * 1024 // 1 MB
+	projectCacheKeyPrefix     = "proj:"
 
-func NewProjectsService(mlpAPIClient mlp.APIClient) ProjectsService {
-	return &projectsService{
-		mlpAPIClient: mlpAPIClient,
-	}
+	// mlpQueryTimeoutSeconds is the timeout that will be used on the API calls to the MLP server.
+	mlpQueryTimeoutSeconds = 30
+)
+
+// ProjectsService provides a set of methods to interact with the MLP / Merlin APIs
+type ProjectsService interface {
+	// List lists available projects, optionally filtered by given project `name`
+	List(ctx context.Context, name string) (mlp.Projects, error)
+	// GetByID gets the project matching the provided id.
+	GetByID(ctx context.Context, projectID int32) (mlp.Project, error)
 }
 
 type projectsService struct {
-	mlpAPIClient mlp.APIClient
+	mlpClient mlp.APIClient
+	cache     *freecache.Cache
 }
 
-func (service *projectsService) List(ctx context.Context, projectName string) (mlp.Projects, error) {
-	return service.mlpAPIClient.ListProjects(ctx, projectName)
+// NewProjectsService returns a service that retrieves information that is shared across MLP projects.
+func NewProjectsService(mlpAPIClient mlp.APIClient) (ProjectsService, error) {
+	svc := &projectsService{
+		mlpClient: mlpAPIClient,
+		cache:     freecache.NewCache(projectCacheSizeBytes),
+	}
+
+	err := svc.refreshProjects(context.Background())
+	if err != nil {
+		return nil, err
+	}
+	return svc, nil
 }
 
-func (service *projectsService) GetByID(ctx context.Context, projectID int32) (mlp.Project, error) {
-	return service.mlpAPIClient.GetProjectByID(ctx, projectID)
+func (ps projectsService) List(ctx context.Context, name string) (mlp.Projects, error) {
+	err := ps.refreshProjects(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	return ps.listProjects(name)
 }
 
-func (service *projectsService) GetByName(ctx context.Context, projectName string) (mlp.Project, error) {
-	return service.mlpAPIClient.GetProjectByName(ctx, projectName)
+// GetByID gets the project matching the provided id.
+// This method will hit the cache first, and if not found, will call Merlin once to get
+// the updated list of projects and refresh the cache, then try to get the value again.
+// If still not found, will return a freecache NotFound error.
+func (ps projectsService) GetByID(ctx context.Context, projectID int32) (mlp.Project, error) {
+	project, err := ps.getProject(projectID)
+	if err != nil {
+		err = ps.refreshProjects(ctx)
+		if err != nil {
+			return mlp.Project{}, err
+		}
+		project, err = ps.getProject(projectID)
+		if err != nil {
+			return mlp.Project{}, err
+		}
+	}
+	return *project, nil
+}
+
+func (ps projectsService) listProjects(name string) (mlp.Projects, error) {
+	projects := mlp.Projects{}
+	// List all items in the cache
+	iter := ps.cache.NewIterator()
+	for entry := iter.Next(); entry != nil; {
+		key := string(entry.Key)
+		if strings.HasPrefix(key, projectCacheKeyPrefix) {
+			var project mlpClient.Project
+			if err := json.Unmarshal(entry.Value, &project); err != nil {
+				return mlp.Projects{}, fmt.Errorf("Malformed project info found in the cache for key %s: %w", key, err)
+			}
+			// If either the name filter is not set or the project's name matches
+			if name == "" || project.Name == name {
+				projects = append(projects, project)
+			}
+		}
+		// Get next item
+		entry = iter.Next()
+	}
+	return projects, nil
+}
+
+func (ps projectsService) getProject(id int32) (*mlp.Project, error) {
+	key := buildProjectKey(id)
+	cachedValue, err := ps.cache.Get([]byte(key))
+	if err != nil {
+		return nil, fmt.Errorf("Project info for id %d not found in the cache", id)
+	}
+	// Unmarshal the data
+	var project mlpClient.Project
+	if err := json.Unmarshal(cachedValue, &project); err != nil {
+		return nil, fmt.Errorf("Malformed project info found in the cache for key %s: %w", key, err)
+	}
+
+	mlpProject := mlp.Project(project)
+	return &mlpProject, nil
+}
+
+func (ps projectsService) refreshProjects(ctx context.Context) error {
+	ctx, cancel := context.WithTimeout(ctx, mlpQueryTimeoutSeconds*time.Second)
+	defer cancel()
+
+	projects, err := ps.mlpClient.ListProjects(ctx, "")
+	if err != nil {
+		return err
+	}
+	for _, project := range projects {
+		key := buildProjectKey(project.ID)
+		valueBytes, err := json.Marshal(project)
+		if err != nil {
+			return fmt.Errorf("Error marshaling project data: %w", err)
+		}
+		err = ps.cache.Set([]byte(key), valueBytes, projectCacheExpirySeconds)
+		if err != nil {
+			return fmt.Errorf("Error caching project data: %w", err)
+		}
+	}
+	return nil
+}
+
+func buildProjectKey(id int32) string {
+	return fmt.Sprintf("%s%d", projectCacheKeyPrefix, id)
 }
