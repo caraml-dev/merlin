2add82c76816c11b976f2156a3eabd423f0dd46b
Arief Rahmansyah <arief.rahmansyah@gojek.com>
2023-07-05 14:17:24 +0700

    Fix getting model and model endpoint id for renderin model alert page

--- ui/src/model/ModelDetails.js
+++ ui/src/model/ModelDetails.js
@@ -14,17 +14,13 @@
  * limitations under the License.
  */
 
-import React, { useEffect, useState } from "react";
-import { Route, Routes, useLocation, useParams } from "react-router-dom";
-import {
-  EuiLoadingContent,
-  EuiPageTemplate,
-  EuiSpacer
-} from "@elastic/eui";
-import { get } from "@caraml-dev/ui-lib";
-import { useMerlinApi } from "../hooks/useMerlinApi";
-import { ModelAlert } from "./alert/ModelAlert";
+import { EuiLoadingContent, EuiPageTemplate, EuiSpacer } from "@elastic/eui";
+import React from "react";
+import { Route, Routes, useParams } from "react-router-dom";
 import { featureToggleConfig } from "../config";
+import { useMerlinApi } from "../hooks/useMerlinApi";
+import mocks from "../mocks";
+import { ModelAlert } from "./alert/ModelAlert";
 
 const LoadingContent = () => (
   <EuiPageTemplate.Section>
@@ -34,34 +30,23 @@ const LoadingContent = () => (
 
 export const ModelDetails = () => {
   const { projectId, modelId } = useParams();
-  const location = useLocation();
-  const [model, setModel] = useState(get(location.state, "model"));
-  const [breadcrumbs, setBreadcrumbs] = useState([]);
 
-  const [{ data: models, isLoaded: modelsLoaded }] = useMerlinApi(
-    `/projects/${projectId}/models`,
-    {},
-    [],
-    !model
+  const [{ data: model, isLoaded: modelLoaded }] = useMerlinApi(
+    `/projects/${projectId}/models/${modelId}`,
+    { mock: mocks.model },
+    []
   );
 
-  useEffect(() => {
-    modelsLoaded && setModel(models.find(m => m.id.toString() === modelId));
-  }, [models, modelsLoaded, modelId, setModel]);
-
-  useEffect(() => {
-    model &&
-      setBreadcrumbs([
-        {
-          text: "Models",
-          href: `/merlin/projects/${projectId}/models`
-        },
-        {
-          text: model.name,
-          href: `/merlin/projects/${projectId}/models/${model.id}`
-        }
-      ]);
-  }, [projectId, model]);
+  const breadcrumbs = [
+    {
+      text: "Models",
+      href: `/merlin/projects/${projectId}/models`,
+    },
+    {
+      text: model.name,
+      href: `/merlin/projects/${projectId}/models/${model.id}`,
+    },
+  ];
 
   return (
     <EuiPageTemplate restrictWidth="90%" paddingSize="none">
@@ -71,20 +56,26 @@ export const ModelDetails = () => {
         iconType={"machineLearningApp"}
         pageTitle={model.name}
       />
-      
+
       <EuiSpacer size="l" />
       <EuiPageTemplate.Section color={"transparent"}>
         {featureToggleConfig.alertEnabled && (
           <Routes>
             <Route index element={<LoadingContent default />} />
-            {model && (<Route path="endpoints/:endpointId/alert" element={<ModelAlert
+            {modelLoaded && model && (
+              <Route
                 path="endpoints/:endpointId/alert"
-                breadcrumbs={breadcrumbs}
-                model={model}
-              />} />)}
+                element={
+                  <ModelAlert
+                    path="endpoints/:endpointId/alert"
+                    breadcrumbs={breadcrumbs}
+                    model={model}
+                  />
+                }
+              />
+            )}
           </Routes>
         )}
-       
       </EuiPageTemplate.Section>
       <EuiSpacer size="l" />
     </EuiPageTemplate>
