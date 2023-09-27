ba1ef0effbcec07dc3b42d685d86593cfae71f8f
tkpd-hafizhan <hafizhan.aliady@tokopedia.com>
2023-09-27 19:53:05 +0700

    add more assert

--- python/sdk/test/integration_test.py
+++ python/sdk/test/integration_test.py
@@ -1439,7 +1439,9 @@ def test_standard_transformer_simulate(integration_test_url, use_google_oauth):
         },
     }
 
-    assert resp1 == resp_valid_wo_tracing
-    assert resp2 == resp_valid_w_tracing
     assert isinstance(resp1, dict)
     assert isinstance(resp2, dict)
+    assert ["response"] in resp1.keys()
+    assert ["operation_tracing", "response"] in resp2.keys()
+    assert resp1 == resp_valid_wo_tracing
+    assert resp2 == resp_valid_w_tracing
