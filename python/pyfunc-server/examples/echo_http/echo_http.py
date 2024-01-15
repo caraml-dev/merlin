3663962a45ba6a0ab1bd929ccffaa330d4eb74d0
Arief Rahmansyah <arief.rahmansyah@gojek.com>
2024-01-15 15:48:00 +0700

    Address review: Remove mentioning start_server in example

--- python/pyfunc-server/examples/echo_http/echo_http.py
+++ python/pyfunc-server/examples/echo_http/echo_http.py
@@ -1,7 +1,7 @@
 import logging
 
 import merlin
-from merlin.model import ModelType, PyFuncModel
+from merlin.model import PyFuncModel
 
 
 class EchoModel(PyFuncModel):
@@ -19,15 +19,3 @@ if __name__ == "__main__":
         model_instance=EchoModel(),
         conda_env="env.yaml",
     )
-
-    # Or, if you already have logged existing model version on Merlin,
-    # you can get the latest model version and run it locally:
-    # merlin.set_url("<your merlin server url>")
-    # merlin.set_project("<your project>")
-    # merlin.set_model("<your model name>", ModelType.PYFUNC)
-
-    # versions = merlin.active_model().list_version()
-    # versions.sort(key=lambda v: v.id, reverse=True)
-
-    # last_version = versions[0]
-    # last_version.start_server(debug=True)
