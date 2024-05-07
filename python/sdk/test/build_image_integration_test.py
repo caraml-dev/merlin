0e7b34f65bf2db5a88453836664e5c5760f6db0e
Arief Rahmansyah <arief.rahmansyah@gojek.com>
2024-05-08 03:20:38 +0700

    Fix image building test for batch job

--- python/sdk/test/build_image_integration_test.py
+++ python/sdk/test/build_image_integration_test.py
@@ -1,8 +1,16 @@
-from test.batch_integration_test import IrisClassifier
+from test.batch_integration_test import (
+    MODEL_PATH,
+    MODEL_PATH_ARTIFACT_KEY,
+    IrisClassifier,
+)
 from test.pyfunc_integration_test import EnvVarModel
 
-import merlin
+import joblib
 import pytest
+from sklearn import svm
+from sklearn.datasets import load_iris
+
+import merlin
 from merlin.model import ModelType
 
 
@@ -34,12 +42,18 @@ def test_build_image_batch(integration_test_url, project_name, use_google_oauth)
     merlin.set_project(project_name)
     merlin.set_model("build-image-batch", ModelType.PYFUNC_V2)
 
+    clf = svm.SVC(gamma="scale")
+    iris = load_iris()
+    X, y = iris.data, iris.target
+    clf.fit(X, y)
+    joblib.dump(clf, MODEL_PATH)
+
     with merlin.new_model_version() as v:
         v.log_pyfunc_model(
             model_instance=IrisClassifier(),
             conda_env="test/batch/model/env.yaml",
             code_dir=["test"],
-            artifacts={"model_path": "test/batch/model/model.joblib"},
+            artifacts={MODEL_PATH_ARTIFACT_KEY: MODEL_PATH},
         )
 
     image = merlin.build_image(v)
