0f57f69d92a5ab2eee98c39f52268d6c30ae3909
Tio Pramayudi <itok54@gmail.com>
2024-05-28 12:04:35 +0700

    Only use columns that already defined in inference schema

--- python/observation-publisher/tests/test_prediction_log_consumer.py
+++ python/observation-publisher/tests/test_prediction_log_consumer.py
@@ -7,7 +7,6 @@ from caraml.upi.v1.prediction_log_pb2 import PredictionLog
 from merlin.observability.inference import (BinaryClassificationOutput,
                                             InferenceSchema, ValueType)
 from pandas._testing import assert_frame_equal
-
 from publisher.prediction_log_consumer import log_batch_to_dataframe
 
 
@@ -97,7 +96,7 @@ def test_log_to_dataframe():
             score_threshold=0.5,
         ),
         session_id_column="order_id",
-        row_id_column="driver_id"
+        row_id_column="driver_id",
     )
     input_columns = [
         "acceptance_rate",
@@ -147,10 +146,180 @@ def test_log_to_dataframe():
     )
     expected_df = pd.DataFrame.from_records(
         [
-            [0.8, 24, "FOOD", 0.9, "fraud", "1234", "a", request_timestamp, model_version],
-            [0.5, 2, "RIDE", 0.5, "fraud", "1234", "b", request_timestamp, model_version],
-            [1.0, 13, "CAR", 0.4, "non fraud", "5678", "c", request_timestamp, model_version],
-            [0.4, 60, "RIDE", 0.2, "non fraud", "5678", "d", request_timestamp, model_version],
+            [
+                0.8,
+                24,
+                "FOOD",
+                0.9,
+                "fraud",
+                "1234",
+                "a",
+                request_timestamp,
+                model_version,
+            ],
+            [
+                0.5,
+                2,
+                "RIDE",
+                0.5,
+                "fraud",
+                "1234",
+                "b",
+                request_timestamp,
+                model_version,
+            ],
+            [
+                1.0,
+                13,
+                "CAR",
+                0.4,
+                "non fraud",
+                "5678",
+                "c",
+                request_timestamp,
+                model_version,
+            ],
+            [
+                0.4,
+                60,
+                "RIDE",
+                0.2,
+                "non fraud",
+                "5678",
+                "d",
+                request_timestamp,
+                model_version,
+            ],
+        ],
+        columns=[
+            "acceptance_rate",
+            "minutes_since_last_order",
+            "service_type",
+            "prediction_score",
+            "_prediction_label",
+            "order_id",
+            "driver_id",
+            "request_timestamp",
+            "model_version",
+        ],
+    )
+    assert_frame_equal(prediction_logs_df, expected_df, check_like=True)
+
+
+def test_log_to_dataframe_containing_unnecessary_fields():
+    model_id = "test_model"
+    model_version = "0.1.0"
+    inference_schema = InferenceSchema(
+        feature_types={
+            "acceptance_rate": ValueType.FLOAT64,
+            "minutes_since_last_order": ValueType.INT64,
+            "service_type": ValueType.STRING,
+        },
+        model_prediction_output=BinaryClassificationOutput(
+            prediction_score_column="prediction_score",
+            actual_score_column="actual_score",
+            positive_class_label="fraud",
+            negative_class_label="non fraud",
+            score_threshold=0.5,
+        ),
+        session_id_column="order_id",
+        row_id_column="driver_id",
+    )
+    input_columns = [
+        "acceptance_rate",
+        "distance",
+        "driver_id",
+        "minutes_since_last_order",
+        "order_id",
+        "service_type",
+    ]
+    output_columns = ["prediction_score", "prediction_tag"]
+    request_timestamp = datetime(2021, 1, 1, 0, 0, 0)
+    prediction_logs = [
+        new_prediction_log(
+            session_id="1234",
+            model_id=model_id,
+            model_version=model_version,
+            input_columns=input_columns,
+            input_data=[
+                [0.8, 11.2, 1, 24, "FOOD_1", "FOOD"],
+                [0.5, 10.1, 11, 2, "RIDE_11", "RIDE"],
+            ],
+            output_columns=output_columns,
+            output_data=[
+                [0.9, "ok"],
+                [0.5, "neutral"],
+            ],
+            request_timestamp=request_timestamp,
+            row_ids=["a", "b"],
+        ),
+        new_prediction_log(
+            session_id="5678",
+            model_id=model_id,
+            model_version=model_version,
+            input_columns=input_columns,
+            input_data=[
+                [1.0, 2.2, 1, 13, "CAR_1", "CAR"],
+                [0.4, 3.3, 11, 60, "RIDE_11", "RIDE"],
+            ],
+            output_columns=output_columns,
+            output_data=[
+                [0.4, "bad"],
+                [0.2, "bad"],
+            ],
+            request_timestamp=request_timestamp,
+            row_ids=["c", "d"],
+        ),
+    ]
+    prediction_logs_df = log_batch_to_dataframe(
+        prediction_logs, inference_schema, model_version
+    )
+    expected_df = pd.DataFrame.from_records(
+        [
+            [
+                0.8,
+                24,
+                "FOOD",
+                0.9,
+                "fraud",
+                "1234",
+                "a",
+                request_timestamp,
+                model_version,
+            ],
+            [
+                0.5,
+                2,
+                "RIDE",
+                0.5,
+                "fraud",
+                "1234",
+                "b",
+                request_timestamp,
+                model_version,
+            ],
+            [
+                1.0,
+                13,
+                "CAR",
+                0.4,
+                "non fraud",
+                "5678",
+                "c",
+                request_timestamp,
+                model_version,
+            ],
+            [
+                0.4,
+                60,
+                "RIDE",
+                0.2,
+                "non fraud",
+                "5678",
+                "d",
+                request_timestamp,
+                model_version,
+            ],
         ],
         columns=[
             "acceptance_rate",
@@ -245,7 +414,7 @@ def test_standard_model_log_to_dataframe():
             score_threshold=0.5,
         ),
         session_id_column="order_id",
-        row_id_column="driver_id"
+        row_id_column="driver_id",
     )
     request_timestamp = datetime(2021, 1, 1, 0, 0, 0)
     prediction_logs = [
@@ -285,10 +454,50 @@ def test_standard_model_log_to_dataframe():
     )
     expected_df = pd.DataFrame.from_records(
         [
-            [0.8, 24, "FOOD", 0.9, "fraud", "1234", "a", request_timestamp, model_version],
-            [0.5, 2, "RIDE", 0.5, "fraud", "1234", "b", request_timestamp, model_version],
-            [1.0, 13, "CAR", 0.4, "non fraud", "5678", "c", request_timestamp, model_version],
-            [0.4, 60, "RIDE", 0.2, "non fraud", "5678", "d", request_timestamp, model_version],
+            [
+                0.8,
+                24,
+                "FOOD",
+                0.9,
+                "fraud",
+                "1234",
+                "a",
+                request_timestamp,
+                model_version,
+            ],
+            [
+                0.5,
+                2,
+                "RIDE",
+                0.5,
+                "fraud",
+                "1234",
+                "b",
+                request_timestamp,
+                model_version,
+            ],
+            [
+                1.0,
+                13,
+                "CAR",
+                0.4,
+                "non fraud",
+                "5678",
+                "c",
+                request_timestamp,
+                model_version,
+            ],
+            [
+                0.4,
+                60,
+                "RIDE",
+                0.2,
+                "non fraud",
+                "5678",
+                "d",
+                request_timestamp,
+                model_version,
+            ],
         ],
         columns=[
             "acceptance_rate",
@@ -323,7 +532,7 @@ def test_one_dimensional_prediction_output_to_dataframe():
             score_threshold=0.5,
         ),
         session_id_column="order_id",
-        row_id_column="driver_id"
+        row_id_column="driver_id",
     )
     request_timestamp = datetime(2021, 1, 1, 0, 0, 0)
     prediction_logs = [
@@ -348,8 +557,28 @@ def test_one_dimensional_prediction_output_to_dataframe():
     )
     expected_df = pd.DataFrame.from_records(
         [
-            [0.8, 24, "FOOD", 0.9, "fraud", "1234", "a", request_timestamp, model_version],
-            [0.5, 2, "RIDE", 0.5, "fraud", "1234", "b", request_timestamp, model_version],
+            [
+                0.8,
+                24,
+                "FOOD",
+                0.9,
+                "fraud",
+                "1234",
+                "a",
+                request_timestamp,
+                model_version,
+            ],
+            [
+                0.5,
+                2,
+                "RIDE",
+                0.5,
+                "fraud",
+                "1234",
+                "b",
+                request_timestamp,
+                model_version,
+            ],
         ],
         columns=[
             "acceptance_rate",
