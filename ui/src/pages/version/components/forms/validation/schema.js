5e91806e3c6296fe6cca630a2d2b2eceaf268d80
Ewe Zi Yi <36802364+deadlycoconuts@users.noreply.github.com>
2024-07-23 14:53:41 +0800

    fix(ui): Fix yup validation bugs (#594)
    # Description
    Similar to https://github.com/caraml-dev/turing/pull/387, this PR to
    fixes broken validation schemas caused by the upgrade of the `yup`
    package in PR #591 - these changes can largely be grouped into the
    following:
    
    - The `Schema.when` function has been
    [updated](https://github.com/jquense/yup?tab=readme-ov-file#schemawhenkeys-string--string-builder-object--values-any-schema--schema-schema),
    such that when the builder object (one with the fields `is`, `then`, and
    sometimes `otherwise` defined) is passed as an argument, the `then`
    field accepts ONLY functions of the following signature `(schema:
    Schema) => Schema` instead of `Schema` previously.
    
    Existing schemas that do not follow this convention have been updated
    and turned into arrow functions.
      
      Example:
      ```javascript
      // before
      some_field: yup.string().when("some_other_field", {
        is: "nop",
        then: yup.string().required("this is needed")
      }),
    
      // after
      some_field: yup.string().when("some_other_field", {
        is: "nop",
        then: (_) => yup.string().required("this is needed")
      }),
      ```
    - When using the same `Schema.when` function but passing a function
    directly as an argument instead of the builder object, the signature of
    the expected function has also been updated from `(value, schema)=>
    Schema): Schema` to `(values: any[], schema) => Schema): Schema`.
    
    Existing schemas that do not follow this convention now have been
    updated to reflect the new expected array:
    
      Example:
      ```javascript
      // before
    some_field: yup.string().when("some_other_field", (some_other_field,
    schema) =>
        some_other_field ? yup.string().required("this is needed") : schema
      ),
    
      // after
    some_field: yup.string().when("some_other_field", ([some_other_field],
    schema) =>
        some_other_field ? yup.string().required("this is needed") : schema
      ),
      ```
    ## Unrelated Update
    This PR also bumps up the version of the `caraml-dev/ui-lib` library to
    the latest version.
    
    # Modifications
    - UI bug fixes
    - Update to `caraml-dev/ui-lib` library
    
    # Tests
    <!-- Besides the existing / updated automated tests, what specific
    scenarios should be tested? Consider the backward compatibility of the
    changes, whether corner cases are covered, etc. Please describe the
    tests and check the ones that have been completed. Eg:
    - [x] Deploying new and existing standard models
    - [ ] Deploying PyFunc models
    -->
    
    # Checklist
    - [x] Added PR label
    - [ ] Added unit test, integration, and/or e2e tests
    - [x] Tested locally
    - [ ] Updated documentation
    - [ ] Update Swagger spec if the PR introduce API changes
    - [ ] Regenerated Golang and Python client if the PR introduces API
    changes
    
    # Release Notes
    ```release-note
    NONE
    ```

--- ui/src/pages/version/components/forms/validation/schema.js
+++ ui/src/pages/version/components/forms/validation/schema.js
@@ -2,6 +2,7 @@ import * as yup from "yup";
 
 const cpuRequestRegex = /^(\d{1,3}(\.\d{1,3})?)$|^(\d{2,5}m)$/,
   memRequestRegex = /^\d+(Ei?|Pi?|Ti?|Gi?|Mi?|Ki?)?$/,
+  gpuRequestRegex= /^\d+$/,
   envVariableNameRegex = /^[a-z0-9_]*$/i,
   dockerImageRegex =
     /^([a-z0-9]+(?:[._-][a-z0-9]+)*(?::\d{2,5})?\/)?([a-z0-9]+(?:[._-][a-z0-9]+)*\/)*([a-z0-9]+(?:[._-][a-z0-9]+)*)(?::[a-z0-9]+(?:[._-][a-z0-9]+)*)?$/i;
@@ -19,9 +20,9 @@ const resourceRequestSchema = (maxAllowedReplica) => yup.object().shape({
   gpu_request: yup
     .string()
     .typeError("GPU value is required")
-    .when("gpu_resource_type", {
-      is: (v) => v !== undefined && v !== "None",
-      then: yup.string().required("GPU value is required"),
+    .when("gpu_name", {
+      is: (v) => v !== undefined,
+      then: (_) => yup.string().matches(gpuRequestRegex, { message:'Valid GPU value is required'}).required("GPU value is required"),
     }),
   min_replica: yup
     .number()
@@ -39,7 +40,7 @@ const resourceRequestSchema = (maxAllowedReplica) => yup.object().shape({
       // eslint-disable-next-line no-template-curly-in-string
       "Max Replicas value has exceeded allowed number of replicas: ${max}"
     )
-    .when("min_replica", (minReplica, schema) =>
+    .when("min_replica", ([minReplica], schema) =>
       minReplica === 0
         ? schema.positive("Max Replica should be positive")
         : schema
@@ -101,7 +102,7 @@ const feastFeaturesSchema = yup.object().shape({
 export const feastInputSchema = yup.object().shape({
   tableName: yup.string().when("isTableNameEditable", {
     is: true,
-    then: yup.string().required("Table name is required"),
+    then: (_) => yup.string().required("Table name is required"),
   }),
   project: yup.string().required("Project name is required"),
   source: yup.string().required("Source is required"),
@@ -142,7 +143,7 @@ const tablesInputSchema = yup.object().shape({
   }),
   columns: yup
     .array()
-    .when("baseTable.fromTable.tableName", (tableName, schema) => {
+    .when("baseTable.fromTable.tableName", ([tableName], schema) => {
       return tableName !== undefined ? yup.array(variableInputSchema) : schema;
     }),
 });
@@ -172,14 +173,14 @@ const tableTransformationStep = yup.object().shape({
     .of(yup.string())
     .when("operation", {
       is: (v) => v !== undefined && v === "dropColumns",
-      then: yup.array().required("List of columns to be deleted is required"),
+      then: (_) => yup.array().required("List of columns to be deleted is required"),
     }),
   selectColumns: yup
     .array()
     .of(yup.string())
     .when("operation", {
       is: (v) => v !== undefined && v === "selectColumns",
-      then: yup.array().required("List of columns to be selected is required"),
+      then: (_) => yup.array().required("List of columns to be selected is required"),
     }),
 });
 
@@ -208,7 +209,7 @@ const transformationPipelineSchema = yup.object().shape({
         .when("how", {
           is: (v) =>
             v !== undefined && v !== "" && v !== "CROSS" && v !== "CONCAT",
-          then: yup.array().required("On Columns is required"),
+          then: (_) => yup.array().required("On Columns is required"),
         }),
     }),
 });
