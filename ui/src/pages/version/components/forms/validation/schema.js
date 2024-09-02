e082298ea7b4b8b42867910fe8796240fab446a4
bthari <smartnastiti.bthari@gmail.com>
2024-09-02 12:52:15 +0700

    add input key for string schema validation

--- ui/src/pages/version/components/forms/validation/schema.js
+++ ui/src/pages/version/components/forms/validation/schema.js
@@ -67,13 +67,13 @@ const environmentVariableSchema = yup.object().shape({
   value: yup.string(),
 });
 
-const stringValueSchema = (name) => yup.mixed().test(
+const stringValueSchema = (name, key) => yup.mixed().test(
   `value input schema`,
   `${name} must be a \`string\` but the final value was $\{value}`,
   (value, { createError }) => {
     try {
-      if (typeof value == "object" && "jsonPath" in value) {
-        return yup.string().required(`${name} is required`).validateSync(value["jsonPath"])
+      if (typeof value == "object" && key in value) {
+        return yup.string().required(`${name} is required`).validateSync(value[key])
       }
       return yup.string().required(`${name} is required`).validateSync(value)
     } catch (e) {
@@ -107,7 +107,7 @@ const feastEntitiesSchema = yup.object().shape({
   name: yup.string().required("Entity Name is required"),
   valueType: yup.string().required("Entity Value Type is required"),
   fieldType: yup.string().required("Input Type is required"),
-  field: stringValueSchema("Input Value"),
+  field: stringValueSchema("Input Value", "jsonPath"),
 });
 
 const feastFeaturesSchema = yup.object().shape({
@@ -128,7 +128,7 @@ export const feastInputSchema = yup.object().shape({
 const variableInputSchema = yup.object().shape({
   name: yup.string().required("Name is required"),
   type: yup.string().required("Type is required"),
-  value: stringValueSchema("Value"),
+  value: stringValueSchema("Value", "jsonPath"),
 });
 
 const tablesInputSchema = yup.object().shape({
