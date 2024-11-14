36b553c92ad8c9e9e6c68a8163fbbf52f75749df
ewezy <ziyi.ewe@gojek.com>
2024-11-14 12:27:38 +0800

    Update mlflow version in sdk

--- python/sdk/setup.py
+++ python/sdk/setup.py
@@ -15,6 +15,8 @@
 
 import imp
 import os
+import pathlib
+import pkg_resources
 
 from setuptools import find_packages, setup
 
@@ -22,49 +24,17 @@ version = imp.load_source(
     "merlin.version", os.path.join("merlin", "version.py")
 ).VERSION
 
-REQUIRES = [
-    "boto3>=1.9.84",
-    "caraml-upi-protos>=0.3.1",
-    "certifi>=2017.4.17",
-    "Click>=7.0,<8.1.4",
-    "cloudpickle==2.0.0",  # used by mlflow
-    "cookiecutter>=1.7.2",
-    "dataclasses-json>=0.5.2",  # allow Flyte version 1.2.0 or above to import Merlin SDK
-    "docker<=6.1.3",
-    "GitPython>=3.1.40",
-    "google-cloud-storage>=1.19.0",
-    "protobuf>=3.12.0,<5.0.0",  # Determined by the mlflow dependency
-    "mlflow==1.26.1",
-    "PyPrind>=2.11.2",
-    "python_dateutil>=2.5.3",
-    "PyYAML>=5.4",
-    "six>=1.10",
-    "urllib3>=1.26",
-    "numpy<=1.23.5",  # Temporary pin numpy due to https://numpy.org/doc/stable/release/1.20.0-notes.html#numpy-1-20-0-release-notes
-    "caraml-auth-google==0.0.0.post7",
-    "pydantic==2.5.3"
-]
+with pathlib.Path("requirements.txt").open() as requirements_txt:
+    requirements = [
+        str(requirement)
+        for requirement in pkg_resources.parse_requirements(requirements_txt)
+    ]
 
-TEST_REQUIRES = [
-    "google-cloud-bigquery-storage>=0.7.0",
-    "google-cloud-bigquery>=1.18.0",
-    "joblib>=0.13.0,<1.2.0",  # >=1.2.0 upon upgrade of kserve's version
-    "mypy>=0.812",
-    "pytest-cov",
-    "pytest-dependency",
-    "pytest-xdist",
-    "pytest",
-    "recursive-diff>=1.0.0",
-    "requests",
-    "scikit-learn>=1.1.2",
-    "types-python-dateutil",
-    "types-PyYAML",
-    "types-six",
-    "types-protobuf",
-    "urllib3-mock>=0.3.3",
-    "xarray",
-    "xgboost==1.6.2",
-]
+with pathlib.Path("requirements.test.txt").open() as test_requirements_test:
+    test_requirements = [
+        str(requirement)
+        for requirement in pkg_resources.parse_requirements(test_requirements_test)
+    ]
 
 setup(
     name="merlin-sdk",
@@ -75,10 +45,10 @@ setup(
     packages=find_packages(),
     package_data={"merlin": ["docker/pyfunc.Dockerfile", "docker/standard.Dockerfile"]},
     zip_safe=True,
-    install_requires=REQUIRES,
+    install_requires=requirements,
     setup_requires=["setuptools_scm"],
-    tests_require=TEST_REQUIRES,
-    extras_require={"test": TEST_REQUIRES},
+    tests_require=test_requirements,
+    extras_require={"test": test_requirements},
     python_requires=">=3.8,<3.11",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
