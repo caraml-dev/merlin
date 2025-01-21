e70009b719515d5504142c8db9065ea4ca308f6d
shydefoo <shidefoo@gmail.com>
2025-01-21 16:14:44 +0800

    Make simple protobuf change and regenerate

--- python/batch-predictor/pkg/github.com/caraml-dev/merlin-pyspark-app/pkg/spec/prediction_job.pb.json.go
+++ python/batch-predictor/pkg/github.com/caraml-dev/merlin-pyspark-app/pkg/spec/prediction_job.pb.json.go
@@ -56,6 +56,22 @@ func (msg *GcsSource) UnmarshalJSON(b []byte) error {
 }
 
 // MarshalJSON implements json.Marshaler
+func (msg *MaxComputeSource) MarshalJSON() ([]byte, error) {
+	return protojson.MarshalOptions{
+		UseEnumNumbers:  false,
+		EmitUnpopulated: false,
+		UseProtoNames:   false,
+	}.Marshal(msg)
+}
+
+// UnmarshalJSON implements json.Unmarshaler
+func (msg *MaxComputeSource) UnmarshalJSON(b []byte) error {
+	return protojson.UnmarshalOptions{
+		DiscardUnknown: false,
+	}.Unmarshal(b, msg)
+}
+
+// MarshalJSON implements json.Marshaler
 func (msg *Model) MarshalJSON() ([]byte, error) {
 	return protojson.MarshalOptions{
 		UseEnumNumbers:  false,
