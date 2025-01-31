e53f67a0b21ed64d191d4f7dab2c9635f0e157b3
eweziyi <ziyi.ewe@gojek.com>
2025-01-31 14:36:56 +0800

    Update create secret method

--- api/cluster/secret.go
+++ api/cluster/secret.go
@@ -12,20 +12,30 @@ import (
 )
 
 func (c *controller) createSecret(ctx context.Context, secretName string, namespace string, secretMap map[string]string) (string, error) {
-	secret, err := c.clusterClient.Secrets(namespace).Create(ctx, &corev1.Secret{
+	_, err := c.clusterClient.Secrets(namespace).Get(ctx, secretName, metav1.GetOptions{})
+	secretConfig := corev1.Secret{
 		ObjectMeta: metav1.ObjectMeta{
 			Name:      secretName,
 			Namespace: namespace,
 		},
 		StringData: secretMap,
 		Type:       corev1.SecretTypeOpaque,
-	}, metav1.CreateOptions{})
+	}
 
+	if err == nil {
+		secret, err := c.clusterClient.Secrets(namespace).Update(ctx, &secretConfig, metav1.UpdateOptions{})
+		if err != nil {
+			log.Errorf("failed updating secret %s in namespace %s: %v", secretName, namespace, err)
+			return "", fmt.Errorf("failed updating secret %s in namespace %s: %w", secretName, namespace, err)
+		}
+		return secret.Name, nil
+	}
+
+	secret, err := c.clusterClient.Secrets(namespace).Create(ctx, &secretConfig, metav1.CreateOptions{})
 	if err != nil {
 		log.Errorf("failed creating secret %s in namespace %s: %v", secretName, namespace, err)
 		return "", fmt.Errorf("failed creating secret %s in namespace %s: %w", secretName, namespace, err)
 	}
-
 	return secret.Name, nil
 }
 
