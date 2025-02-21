caraml-dev/merlin

commit 71d93a05284d46bd693dcf464d15bfd7f6be15f7

feat(ui): Add secret management UI (#627)
# Description
Following up on the new feature allowing users to configure MLP secrets
in their model services/batch jobs as introduced in PR #625, this PR
introduces UI changes to complement that feature, allowing users to
configure secrets for model services and batch jobs, as well as to view
them in the details page view.

## Model Services
### Deployment Page
![Screenshot 2025-02-06 at 5 02
35 PM](https://github.com/user-attachments/assets/c386510d-cf94-4ea5-b303-80ba7a0c962b)

### Details Page
![Screenshot 2025-02-06 at 5 00
52 PM](https://github.com/user-attachments/assets/0af9d74e-a3b4-4ac0-b80f-2b8633f14cfc)

## Batch Jobs
### Submission Page
![Screenshot 2025-02-06 at 5 02
05 PM](https://github.com/user-attachments/assets/3dadcd4f-8c06-42a1-a797-240119bd1292)

### Details Page
![Screenshot 2025-02-06 at 5 01
03 PM](https://github.com/user-attachments/assets/0f85ba4a-afbb-4027-a5e5-b6284ded5ec1)

Note that the schema validation unfortunately only exists for model
service deployments and not batch jobs, and as such this PR does not add
additional new validation checks for the `secrets` field for batch jobs
triggered via the UI.

Some of the changes to the column widths/proportions were done to
improve consistency in the component sizes throughout the UI.

## Additional Change
This PR also includes a tiny fix to the API server to ensure that k8s
secrets for each Merlin transformer is only created when the transformer
is actually enabled (as opposed to when the transformer config exists).

# Modifications
- `api/cluster/controller.go` - Fix for the API server to not create
transformer secrets unnecessarily
- `ui/src/components/SecretsConfigTable.js` - New component to display
secrets in the details view
- `ui/src/pages/job/form/components/SecretsForm.js` - New form for users
to configure secrets for batch jobs

ui/src/pages/job/form/components/SecretsForm.js
@@ -21,12 +21,15 @@ import {
   EuiFieldText,
   EuiInMemoryTable,
   EuiTitle,
+  EuiSuperSelect
 } from "@elastic/eui";
 import PropTypes from "prop-types";
+import { useParams  } from "react-router-dom";
+import { useMerlinApi } from "../../../../hooks/useMerlinApi";
 
-require("../../../../assets/scss/EnvironmentVariables.scss");
+require("../../../../assets/scss/Secrets.scss");
 
-export const EnvironmentVariablesForm = ({ variables, onChange }) => {
+export const SecretsForm = ({ variables, onChange }) => {
   const [items, setItems] = useState([
     ...variables.map((v, idx) => ({ idx, ...v })),
     { idx: variables.length },
@@ -38,7 +41,7 @@ export const EnvironmentVariablesForm = ({ variables, onChange }) => {
     const trimmedVars = [
       ...items
         .slice(0, items.length - 1)
-        .map((item) => ({ name: item.name.trim(), value: item.value })),
+        .map((item) => ({ mlp_secret_name: item.mlp_secret_name.trim(), env_var_name: item.env_var_name })),
     ];
     if (JSON.stringify(variables) !== JSON.stringify(trimmedVars)) {
       setVars(trimmedVars);
@@ -50,46 +53,79 @@ export const EnvironmentVariablesForm = ({ variables, onChange }) => {
     setItems([...items.map((v, idx) => ({ ...v, idx }))]);
   };
 
-  const onChangeRow = (idx, field) => {
+  const onChangeMLPSecretName = (idx) => {
     return (e) => {
-      items[idx] = { ...items[idx], [field]: e.target.value };
+      items[idx] = { ...items[idx], mlp_secret_name: e };
 
       setItems((_) =>
-        field === "name" &&
-        items[items.length - 1].name &&
-        items[items.length - 1].name.trim()
+        items[items.length - 1].mlp_secret_name &&
+        items[items.length - 1].mlp_secret_name.trim()
           ? [...items, { idx: items.length }]
           : [...items],
       );
     };
   };
 
+  const onChangeEnvironmentVariableName = (idx) => {
+    return (e) => {
+      items[idx] = { ...items[idx], env_var_name: e.target.value };
+      setItems((_) => [...items]);
+    };
+  };
+
+  const { projectId } = useParams();
+  const [options, setOptions] = useState([]);
+
+  const [{ data: secrets }] = useMerlinApi(
+    `/projects/${projectId}/secrets`,
+    {},
+    [],
+  );
+
+  useEffect(() => {
+    if (secrets) {
+      const options = [];
+      secrets
+        .sort((a, b) => (a.name > b.name ? -1 : 1))
+        .forEach((secret) => {
+          options.push({
+            value: secret.name,
+            inputDisplay: secret.name,
+            textWrap: "truncate",
+          });
+        });
+      setOptions(options);
+    }
+  }, [secrets]);
+
   const columns = [
     {
-      name: "Name",
-      field: "name",
+      name: "MLP Secret Name",
+      field: "mlp_secret_name",
       width: "45%",
+      textOnly: false,
       render: (name, item) => (
-        <EuiFieldText
-          controlOnly
-          className="inlineTableInput"
-          placeholder="Name"
-          value={name || ""}
-          onChange={onChangeRow(item.idx, "name")}
+        <EuiSuperSelect
+          placeholder={"Select MLP secret"}
+          compressed={true}
+          options={options}
+          valueOfSelected={name}
+          onChange={onChangeMLPSecretName(item.idx)}
+          hasDividers
         />
       ),
     },
     {
-      name: "Value",
-      field: "value",
+      name: "Environment Variable Name",
+      field: "env_var_name",
       width: "45%",
       render: (value, item) => (
         <EuiFieldText
           controlOnly
           className="inlineTableInput"
-          placeholder="Value"
+          placeholder="Environment Variable Name"
           value={value || ""}
-          onChange={onChangeRow(item.idx, "value")}
+          onChange={onChangeEnvironmentVariableName(item.idx)}
         />
       ),
     },
@@ -118,11 +154,11 @@ export const EnvironmentVariablesForm = ({ variables, onChange }) => {
   return (
     <Fragment>
       <EuiTitle size="xs">
-        <h4>Environment Variables</h4>
+        <h4>Secrets</h4>
       </EuiTitle>
 
       <EuiInMemoryTable
-        className="EnvVariables"
+        className="Secrets"
         columns={columns}
         items={items}
       />
@@ -130,7 +166,7 @@ export const EnvironmentVariablesForm = ({ variables, onChange }) => {
   );
 };
 
-EnvironmentVariablesForm.propTypes = {
+SecretsForm.propTypes = {
   variables: PropTypes.array,
   onChange: PropTypes.func,
 };

ui/src/pages/version/components/forms/components/SecretsPanel.js
@@ -1,15 +1,41 @@
-import React from "react";
-import { EuiButtonIcon, EuiFieldText, EuiSpacer } from "@elastic/eui";
+import React, { useEffect, useState } from "react";
+import { EuiButtonIcon, EuiFieldText, EuiSpacer, EuiSuperSelect } from "@elastic/eui";
 import { InMemoryTableForm, useOnChangeHandler } from "@caraml-dev/ui-lib";
 import { Panel } from "./Panel";
-import { STANDARD_TRANSFORMER_CONFIG_ENV_NAME } from "../../../../../services/transformer/TransformerConfig";
+import { useMerlinApi } from "../../../../../hooks/useMerlinApi";
+import { useParams } from "react-router-dom";
+import "./SecretsPanel.scss"
 
-export const EnvVariablesPanel = ({
+export const SecretsPanel = ({
   variables,
   onChangeHandler,
   errors = {}
 }) => {
   const { onChange } = useOnChangeHandler(onChangeHandler);
+  const { projectId } = useParams();
+  const [options, setOptions] = useState([]);
+
+  const [{ data: secrets }] = useMerlinApi(
+    `/projects/${projectId}/secrets`,
+    {},
+    [],
+  );
+
+  useEffect(() => {
+    if (secrets) {
+      const options = [];
+      secrets
+        .sort((a, b) => (a.name > b.name ? -1 : 1))
+        .forEach((secret) => {
+          options.push({
+            value: secret.name,
+            inputDisplay: secret.name,
+            textwrap: "truncate"
+          });
+        });
+      setOptions(options);
+    }
+  }, [secrets]);
 
   const items = [
     ...variables.map((v, idx) => ({ idx, ...v })),
@@ -32,30 +58,31 @@ export const EnvVariablesPanel = ({
 
   const columns = [
     {
-      name: "Name",
-      field: "name",
+      name: "MLP Secret Name",
+      field: "mlp_secret_name",
       width: "45%",
       render: (name, item) => (
-        <EuiFieldText
-          controlOnly
-          className="inlineTableInput"
-          placeholder="Name"
-          value={name || ""}
-          onChange={e => onChange(`${item.idx}.name`)(e.target.value)}
+        <EuiSuperSelect
+          placeholder={"Select MLP secret"}
+          compressed={true}
+          options={options}
+          valueOfSelected={name}
+          onChange={e => onChange(`${item.idx}.mlp_secret_name`)(e)}
+          hasDividers
         />
       )
     },
     {
-      name: "Value",
-      field: "value",
+      name: "Environment Variable Name",
+      field: "env_var_name",
       width: "45%",
       render: (value, item) => (
         <EuiFieldText
           controlOnly
           className="inlineTableInput"
-          placeholder="Value"
+          placeholder="Environment Variable Name"
           value={value || ""}
-          onChange={e => onChange(`${item.idx}.value`)(e.target.value)}
+          onChange={e => onChange(`${item.idx}.env_var_name`)(e.target.value)}
         />
       )
     },
@@ -81,17 +108,13 @@ export const EnvVariablesPanel = ({
   ];
 
   return (
-    <Panel title="Environment Variables">
+    <Panel title="Secrets">
       <EuiSpacer size="xs" />
       <InMemoryTableForm
         columns={columns}
         rowProps={getRowProps}
-        items={items.filter(
-          v =>
-            v.name !== "MODEL_NAME" &&
-            v.name !== "MODEL_DIR" &&
-            v.name !== STANDARD_TRANSFORMER_CONFIG_ENV_NAME
-        )}
+        className={"Secrets"}
+        items={items}
         errors={errors}
         renderErrorHeader={key => `Row ${parseInt(key) + 1}`}
       />

api/cluster/controller.go
@@ -536,7 +536,7 @@ func (c *controller) createSecrets(ctx context.Context, modelService *models.Ser
 		return fmt.Errorf("failed creating secret for model %s in namespace %s: %w", modelService.Name, modelService.Namespace, err)
 	}
 
-	if modelService.Transformer != nil {
+	if modelService.Transformer != nil && modelService.Transformer.Enabled {
 		transformerSecretName := fmt.Sprintf("%s-transformer", modelService.Name)
 		err = c.createSecretForComponent(ctx, transformerSecretName, modelService.Transformer.Secrets, modelService.Namespace, projectID)
 		if err != nil {

ui/src/assets/scss/Secrets.scss
@@ -1,0 +1,38 @@
+/**
+ * Copyright 2020 The Merlin Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+.Secrets {
+  .euiTableRowCell > .euiTableCellContent {
+    display: block;
+  }
+}
+
+input[type="text"].inlineTableInput {
+  float: left;
+  padding: 0;
+  font-size: inherit !important;
+  line-height: inherit !important;
+  font-family: inherit !important;
+  height: inherit !important;
+  transition: inherit !important;
+  background-color: inherit !important;
+  box-shadow: none !important;
+  min-width: 90px;
+
+  &:focus {
+    background-size: 0 100%;
+  }
+}

ui/src/components/SecretsConfigTable.js
@@ -17,19 +17,18 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { EuiInMemoryTable, EuiText } from "@elastic/eui";
-import { STANDARD_TRANSFORMER_CONFIG_ENV_NAME } from "../services/transformer/TransformerConfig";
 
-export const EnvVarsConfigTable = ({ variables = [] }) => {
+export const SecretsConfigTable = ({ variables = [] }) => {
   const columns = [
     {
-      field: "name",
-      name: "Name",
+      field: "mlp_secret_name",
+      name: "MLP Secret Name",
       width: "40%",
       sortable: true
     },
     {
-      field: "value",
-      name: "Value",
+      field: "env_var_name",
+      name: "Environment Variable Name",
       width: "60%",
       sortable: true
     }
@@ -37,9 +36,7 @@ export const EnvVarsConfigTable = ({ variables = [] }) => {
 
   return variables.length ? (
     <EuiInMemoryTable
-      items={variables.filter(
-        v => v.name !== STANDARD_TRANSFORMER_CONFIG_ENV_NAME
-      )}
+      items={variables}
       columns={columns}
       itemId="name"
     />
@@ -50,6 +47,6 @@ export const EnvVarsConfigTable = ({ variables = [] }) => {
   );
 };
 
-EnvVarsConfigTable.propTypes = {
+SecretsConfigTable.propTypes = {
   variables: PropTypes.array.isRequired
 };

ui/src/pages/job/components/JobConfig.js
@@ -1,5 +1,6 @@
 import { EuiFlexGroup, EuiFlexItem } from "@elastic/eui";
 import { EnvVarsConfigTable } from "../../../components/EnvVarsConfigTable";
+import { SecretsConfigTable } from "../../../components/SecretsConfigTable";
 import {
   ConfigSection,
   ConfigSectionPanel,
@@ -35,10 +36,20 @@ const JobConfig = ({ job }) => {
           <EuiFlexGroup gutterSize="xl">
             <EuiFlexItem>
               <ConfigSectionPanel>
-                <ConfigSectionPanelTitle title="Environment Variables" />
-                <EnvVarsConfigTable
-                  variables={job.config.env_vars ? job.config.env_vars : []}
-                />
+                <EuiFlexGroup direction="column" gutterSize="m">
+                  <EuiFlexItem>
+                    <ConfigSectionPanelTitle title="Environment Variables" />
+                    <EnvVarsConfigTable
+                      variables={job.config.env_vars ? job.config.env_vars : []}
+                    />
+                  </EuiFlexItem>
+                  <EuiFlexItem>
+                    <ConfigSectionPanelTitle title="Secrets" />
+                    <SecretsConfigTable
+                      variables={job.config.secrets ? job.config.secrets : []}
+                    />
+                  </EuiFlexItem>
+                </EuiFlexGroup>
               </ConfigSectionPanel>
             </EuiFlexItem>
 

ui/src/pages/job/components/ResourcesConfigTable.js
@@ -38,7 +38,7 @@ export const ResourcesConfigTable = ({
       compressed
       type="responsiveColumn"
       listItems={items}
-      columnWidths={[1, 1]}
+      columnWidths={[2, 4]}
     />
   );
 };

ui/src/pages/job/form/JobFormOthers.js
@@ -26,6 +26,7 @@ import PropTypes from "prop-types";
 import React, { Fragment, useContext } from "react";
 import { ImageBuilderSection } from "../../version/components/forms/components/ImageBuilderSection";
 import { EnvironmentVariablesForm } from "./components/EnvironmentVariablesForm";
+import { SecretsForm } from "./components/SecretsForm";
 import { ModelVersionSelect } from "./components/ModelVersionsSelect";
 import { ResourceRequestForm } from "./components/ResourceRequestForm";
 import { ServiceAccountSelect } from "./components/ServiceAccountSelect";
@@ -38,6 +39,7 @@ export const JobFormOthers = ({ versions, isSelectVersionDisabled }) => {
     setServiceAccountName,
     setResourceRequest,
     setEnvVars,
+    setSecrets,
     onChangeHandler,
   } = useContext(JobFormContext);
   const { onChange } = useOnChangeHandler(onChangeHandler);
@@ -49,7 +51,7 @@ export const JobFormOthers = ({ versions, isSelectVersionDisabled }) => {
           <EuiFlexItem>
             <ModelVersionSelect
               isDisabled={isSelectVersionDisabled}
-              selected={job.version_id}
+              selected={job.version_id.toString()}
               versions={versions}
               onChange={(selected) => {
                 setVersionId(selected);
@@ -77,11 +79,19 @@ export const JobFormOthers = ({ versions, isSelectVersionDisabled }) => {
       />
 
       <EuiSpacer size="xl" />
+
       <EuiFlexGroup>
-        <EuiFlexItem style={{ maxWidth: 400 }}>
+        <EuiFlexItem>
           <EnvironmentVariablesForm
             variables={job.config.env_vars}
             onChange={setEnvVars}
+          />
+        </EuiFlexItem>
+
+        <EuiFlexItem>
+          <SecretsForm
+            variables={job.config.secrets}
+            onChange={setSecrets}
           />
         </EuiFlexItem>
       </EuiFlexGroup>

commit 1beb8b992bc0234da59d3fd2bbcfee3264b52629

fix(ui): Fix secrets component in batch prediction job creation page (#632)
# Description
This PR fixes a bug with the new UI changes introduced in PR #627,
whereby attempting to redeploy a batch job in a terminal state
(succeeded or failed) that does not have any user secrets configured
(`nil`) will cause the UI to crash. This PR fixes this bug by setting
the default value of the `secrets` field to be `[]` by default.

Another minor change in this PR is also made to remove the redundant
`textwrap` option (this is not recognised and does nothing) that had
been accidentally introduced in the same PR quoted above.

# Modifications
- `ui/src/pages/job/form/components/SecretsForm.js` - Removal of
`textwrap` option
- `ui/src/pages/version/components/forms/components/SecretsPanel.js` -
Removal of `textwrap` option
- `ui/src/services/job/Job.js` - Addition of steps to ensure the
`secrets` field gets intialised as `[]` by default

# Tests
<!-- Besides the existing / updated automated tests, what specific
scenarios should be tested? Consider the backward compatibility of the
changes, whether corner cases are covered, etc. Please describe the
tests and check the ones that have been completed. Eg:
- [x] Deploying new and existing standard models
- [ ] Deploying PyFunc models
-->

# Checklist
- [x] Added PR label
- [ ] Added unit test, integration, and/or e2e tests
- [x] Tested locally
- [ ] Updated documentation
- [ ] Update Swagger spec if the PR introduce API changes
- [ ] Regenerated Golang and Python client if the PR introduces API
changes

# Release Notes
<!--
Does this PR introduce a user-facing change?
If no, just write "NONE" in the release-note block below.
If yes, a release note is required. Enter your extended release note in
the block below.
If the PR requires additional action from users switching to the new
release, include the string "action required".

For more information about release notes, see kubernetes' guide here:

ui/src/pages/job/form/components/SecretsForm.js
@@ -91,7 +91,6 @@ export const SecretsForm = ({ variables, onChange }) => {
           options.push({
             value: secret.name,
             inputDisplay: secret.name,
-            textWrap: "truncate",
           });
         });
       setOptions(options);

ui/src/pages/version/components/forms/components/SecretsPanel.js
@@ -30,7 +30,6 @@ export const SecretsPanel = ({
           options.push({
             value: secret.name,
             inputDisplay: secret.name,
-            textwrap: "truncate"
           });
         });
       setOptions(options);

ui/src/services/job/Job.js
@@ -39,6 +39,8 @@ export class Job {
 
       env_vars: [],
 
+      secrets: [],
+
       job_config: {
         model: {
           type: "",
@@ -86,6 +88,10 @@ export class Job {
   static from(json) {
     if (!json.config.env_vars) {
       json.config.env_vars = [];
+    }
+
+    if (!json.config.secrets) {
+      json.config.secrets = [];
     }
 
     return objectAssignDeep(new Job(), json);