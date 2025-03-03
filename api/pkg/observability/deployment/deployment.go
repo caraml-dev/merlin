da6fbb2046180b5a18874f131e6025c27c5ab169
Vinoth Kumar Sambath <s.vinothkumar@gojek.com>
2025-03-03 19:38:08 +0800

    add mirrored topic prefix

--- api/pkg/observability/deployment/deployment.go
+++ api/pkg/observability/deployment/deployment.go
@@ -303,7 +303,11 @@ func (c *deployer) createSecretSpec(data *models.WorkerData) (*corev1.Secret, er
 		ObservationSource: &ObserVationSource{
 			Type: Kafka,
 			Config: &KafkaSource{
-				Topic:                    data.TopicSource,
+				/*
+					Models in tencent cluster ingest to kafka in tencent. The publiser-observer (consumer) in alicloud reads the mirrored kafka
+					topic. The kafka mirrored from tencent in alicloud has a prefix
+				*/
+				Topic:                    fmt.Sprintf("%s%s", c.consumerConfig.KafkaConsumer.MirroredTopicPrefix, data.TopicSource),
 				BootstrapServers:         c.consumerConfig.KafkaConsumer.Brokers,
 				GroupID:                  c.consumerConfig.KafkaConsumer.GroupID,
 				BatchSize:                c.consumerConfig.KafkaConsumer.BatchSize,
