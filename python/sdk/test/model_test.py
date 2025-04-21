4c3302d4f3595c814315635b6eaace6e11fefa09
Muhammad Naufal Andika Natsir Putra <muhammad.putra@gojek.com>
2025-04-21 15:41:50 +0700

    feat: adjust test_deploy_default_env

--- python/sdk/test/model_test.py
+++ python/sdk/test/model_test.py
@@ -734,3 +734,90 @@ class TestModelVersion:
             assert endpoint.deployment_mode == DeploymentMode.SERVERLESS
             assert endpoint.autoscaling_policy.metrics_type == MetricsType.CPU_UTILIZATION
             assert endpoint.autoscaling_policy.target_value == 10
+            
+    def test_deploy_default_env(self, version):
+        with patch("urllib3.PoolManager.request") as mock_request:
+            mock_response_1 = MagicMock()
+            mock_response_1.method = "GET"
+            mock_response_1.status = 200
+            mock_response_1.path = "/v1/models/1/versions/1/endpoint"
+            mock_response_1.data = json.dumps([]).encode('utf-8')
+            mock_response_1.headers = {
+                'content-type': 'application/json',
+                'charset': 'utf-8'
+            }
+            
+            mock_response_2 = MagicMock()
+            mock_response_2.method = "GET"
+            mock_response_2.status = 200
+            mock_response_2.path = "/v1/environments"
+            mock_response_2.data = json.dumps([env_2.to_dict()]).encode('utf-8')
+            mock_response_2.headers = {
+                'content-type': 'application/json',
+                'charset': 'utf-8'
+            }
+            
+            mock_request.side_effect = [mock_response_1, mock_response_2]
+            
+            with pytest.raises(ValueError):
+                version.deploy()
+                
+            mock_response_1 = MagicMock()
+            mock_response_1.method = "GET"
+            mock_response_1.status = 200
+            mock_response_1.path = "/v1/environments"
+            mock_response_1.data = json.dumps([env_1.to_dict(), env_2.to_dict()]).encode('utf-8')
+            mock_response_1.headers = {
+                'content-type': 'application/json',
+                'charset': 'utf-8'
+            }
+            
+            mock_response_2 = MagicMock()
+            mock_response_2.method = "GET"
+            mock_response_2.status = 200
+            mock_response_2.path = "/v1/models/1/versions/1/endpoint"
+            mock_response_2.data = json.dumps([]).encode('utf-8')
+            mock_response_2.headers = {
+                'content-type': 'application/json',
+                'charset': 'utf-8'
+            }
+
+            mock_response_3 = MagicMock()
+            mock_response_3.method = "POST"
+            mock_response_3.status = 201
+            mock_response_3.path = "/v1/models/1/versions/1/endpoint"
+            mock_response_3.data = json.dumps(ep1.to_dict()).encode('utf-8')
+            mock_response_3.headers = {
+                'content-type': 'application/json',
+                'charset': 'utf-8'
+            }
+        
+            mock_response_4 = MagicMock()
+            mock_response_4.method = "GET"
+            mock_response_4.status = 200
+            mock_response_4.path = "/v1/models/1/versions/1/endpoint/1234"
+            mock_response_4.data = json.dumps(ep1.to_dict()).encode('utf-8')
+            mock_response_4.headers = {
+                'content-type': 'application/json',
+                'charset': 'utf-8'
+            }
+        
+            mock_response_5 = MagicMock()
+            mock_response_5.method = "GET"
+            mock_response_5.status = 200
+            mock_response_5.path = "/v1/models/1/versions/1/endpoint"
+            mock_response_5.data = json.dumps([ep1.to_dict()]).encode('utf-8')
+            mock_response_5.headers = {
+                'content-type': 'application/json',
+                'charset': 'utf-8'
+            }
+            
+            mock_request.side_effect = [mock_response_1, mock_response_2, mock_response_3, mock_response_4, mock_response_5]
+
+            endpoint = version.deploy()
+
+            assert endpoint.id == ep1.id
+            assert endpoint.status.value == ep1.status
+            assert endpoint.environment_name == ep1.environment_name
+            assert endpoint.environment.cluster == env_1.cluster
+            assert endpoint.environment.name == env_1.name
