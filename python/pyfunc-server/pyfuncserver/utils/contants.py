bae92bd86f5494c923f7f3d5bb168769e2d63217
ewezy <ziyi.ewe@gojek.com>
2025-05-19 11:56:00 +0800

    Remove additional mentions of python 3.8

--- python/pyfunc-server/pyfuncserver/utils/contants.py
+++ python/pyfunc-server/pyfuncserver/utils/contants.py
@@ -11,14 +11,14 @@ If the module is a python package, then you should add it in the
 conda env.yaml that you pass to log_pyfunc method, e.g.:
 
 dependencies:
-- python=3.8
+- python=3.9
 - pip:
   - my-missing-package==1.0
   
 OR if the package is not available in PyPI distribution
 
 dependencies:
-- python=3.8
+- python=3.9
 - my-missing-package=1.0
 
 See: https://docs.conda.io/projects/conda/en/latest/user-guide/tasks/manage-environments.html#creating-an-environment-from-an-environment-yml-file
