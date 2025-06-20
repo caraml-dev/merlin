235e3978c0785c6e00bccf94d48b205e46f1d233
Muhammad Naufal Andika Natsir Putra <muhammad.putra@gojek.com>
2025-06-20 15:47:26 +0700

    feat: remove bigquery sink as sink option

--- api/pkg/observability/deployment/deployment.go
+++ api/pkg/observability/deployment/deployment.go
@@ -262,16 +262,6 @@ func (c *deployer) applySecret(ctx context.Context, data *models.WorkerData) (se
 
 func (c *deployer) createSecretSpec(data *models.WorkerData) (*corev1.Secret, error) {
 	observationSinks := []ObservationSink{}
-	if c.consumerConfig.BigQuerySink.Enabled {
-		observationSinks = append(observationSinks, ObservationSink{
-			Type: BQ,
-			Config: BigQuerySink{
-				Project: c.consumerConfig.BigQuerySink.Project,
-				Dataset: c.consumerConfig.BigQuerySink.Dataset,
-				TTLDays: c.consumerConfig.BigQuerySink.TTLDays,
-			},
-		})
-	}
 	if c.consumerConfig.MaxComputeSink.Enabled {
 		observationSinks = append(observationSinks, ObservationSink{
 			Type: MaxCompute,
