098f57c47da478dfa07ce814548600427b1ddfb7
bthari <smartnastiti.bthari@gmail.com>
2025-09-23 16:08:37 +0700

    chore: change postgres image

--- scripts/e2e/deploy-merlin.sh
+++ scripts/e2e/deploy-merlin.sh
@@ -13,6 +13,9 @@ MERLIN_CHART_VERSION="$5"
 
 TIMEOUT=300s
 
+POSTGRES_IMAGE="bitnami/postgresql"
+POSTGRES_TAG="11.16.0-debian-11-r9"
+
 install_merlin() {
   echo "::group::Merlin Deployment"
 
@@ -28,6 +31,12 @@ install_merlin() {
     --set deployment.image.registry=${DOCKER_REGISTRY} \
     --set deployment.image.repository=merlin \
     --set deployment.image.tag=${VERSION} \
+    --set merlin-postgresql.image.repository=${POSTGRES_IMAGE} \
+    --set merlin-postgresql.image.tag=${POSTGRES_TAG} \
+    --set mlflow-postgresql.image.repository=${POSTGRES_IMAGE} \
+    --set mlflow-postgresql.image.tag=${POSTGRES_TAG} \
+    --set mlp-postgresql.image.repository=${POSTGRES_IMAGE} \
+    --set mlp-postgresql.image.tag=${POSTGRES_TAG} \
     --set rendered.overrides.StandardTransformerConfig.ImageName=${DOCKER_REGISTRY}/merlin-transformer:${VERSION} \
     --set imageBuilder.dockerRegistry=${DOCKER_REGISTRY} \
     --set imageBuilder.predictionJobBaseImages."3\.7\.*".imageName=${DOCKER_REGISTRY}/merlin/merlin-pyspark-base-py37:${VERSION} \
