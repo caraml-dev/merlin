7df2634d855bf7559b8a9a4fa56f5c9b76337c78
vishwajeetpal <vishwajeet.pal@gojek.com>
2025-12-15 12:24:16 +0530

    test: checking e2e tests suit.

--- python/pyfunc-server/pyfuncserver/protocol/upi/server.py
+++ python/pyfunc-server/pyfuncserver/protocol/upi/server.py
@@ -57,7 +57,7 @@ class UPIServer:
             # multiprocessing based on https://github.com/grpc/grpc/tree/master/examples/python/multiprocessing
             workers = []
             for _ in range(self._config.workers - 1):
-                worker = multiprocessing.Process(target=self._run_server)
+                worker = multiprocessing.Process(target=self._run_server_sync)
                 worker.start()
                 workers.append(worker)
         
@@ -67,7 +67,11 @@ class UPIServer:
             publisher = Publisher(kafka_producer, sampler)
             self._predict_service.set_publisher(publisher)
 
-        asyncio.get_event_loop().run_until_complete(self._run_server())
+        asyncio.run(self._run_server())
+
+    def _run_server_sync(self):
+        """Synchronous wrapper to run the async server in a new process."""
+        asyncio.run(self._run_server())
 
     async def _run_server(self):
         """
